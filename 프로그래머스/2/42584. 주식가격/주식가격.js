df8a365babe276a014958fe5247167bc3c6b79a5
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2024-10-14 13:48:36 +0900

    [level 2] Title: 주식가격, Time: 10.43 ms, Memory: 45.1 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,27 +1,27 @@
 function solution(prices) {
-    let answer = [];
-    let n = prices.length;
-    let second = 0;
+  let answer = [];
+  let n = prices.length;
+  let second = 0;
 
-    for (let i = 0; i < n; i++) {
-        if (i === n - 1) {
-            answer.push(0);
-            break;
-        }
-        for (let j = i + 1; j <= n - 1; j++) {
-            if (prices[i] <= prices[j]) {
-                second++;
-            } else if (prices[i] > prices[j]) {
-                answer.push(second + 1);
-                second = 0;
-                break;
-            }
-
-            if (j === n - 1) {
-                answer.push(second);
-                second = 0;
-            }
-        }   
+  for (let i = 0; i < n; i++) {
+    if (i === n - 1) {
+      answer.push(0);
+      break;
     }
-    return answer;
+    for (let j = i + 1; j <= n - 1; j++) {
+      if (prices[i] <= prices[j]) {
+        second++;
+      } else if (prices[i] > prices[j]) {
+        answer.push(second + 1);
+        second = 0;
+        break;
+      }
+
+      if (j === n - 1) {
+        answer.push(second);
+        second = 0;
+      }
+    }
+  }
+  return answer;
 }
