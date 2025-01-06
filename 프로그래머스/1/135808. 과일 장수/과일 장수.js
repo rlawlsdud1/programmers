78a9ccc4f934d8ced4d64d796c2af5a300a3b9db
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-01-06 19:56:18 +0900

    [level 1] Title: 과일 장수, Time: 317.64 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -1,14 +1,26 @@
 function solution(k, m, score) {
-  if (m > score.length) {
-    return 0;
-  }
   let answer = 0;
-  score.sort((a, b) => b - a);
-  for (let i = 0; i < score.length; i += m) {
-    const slicedScore = score.slice(i, i + m);
-    if (slicedScore.length === m) {
-      answer += Math.min(...slicedScore) * m;
+  const boxCount = Math.floor(score.length / m);
+  const sortedScore = score.sort((a, b) => b - a);
+
+  const totalBox = [];
+  let fruitBox = [];
+  for (let i = 0; i < m * boxCount; i++) {
+    fruitBox.push(sortedScore[i]);
+    if ((i % m) + 1 === m) {
+      totalBox.push(fruitBox);
+      fruitBox = [];
     }
   }
+  console.log(sortedScore);
+  console.log(totalBox);
+  for (let i = 0; i < totalBox.length; i++) {
+    answer = answer + getMinValue(totalBox[i]) * m;
+  }
+
   return answer;
+}
+
+function getMinValue(fruitBox) {
+  return fruitBox.sort((a, b) => a - b)[0];
 }
