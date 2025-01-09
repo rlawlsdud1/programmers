07928981b67a28d799cc625d5c1d0167926c4cb7
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-01-09 16:34:46 +0900

    [level 1] Title: 덧칠하기, Time: 3414.17 ms, Memory: 66.3 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
@@ -1,15 +1,11 @@
 function solution(n, m, section) {
-  let count = 0;
-
-  while (section.length) {
-    const idx = section.findIndex((v) => v >= section[0] + m);
-    console.log(idx);
-    count++;
-
+  let answer = 0;
+  while (true) {
+    const idx = section.findIndex((v) => v > section[0] + m - 1);
+    answer++;
     if (idx === -1) {
-      return count;
+      return answer;
     }
-
     section = section.slice(idx);
   }
 }
