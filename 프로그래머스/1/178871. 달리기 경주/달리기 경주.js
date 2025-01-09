d060f247ab66fb069e08748bc9d4f46c96314412
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-01-09 16:13:56 +0900

    [level 1] Title: 달리기 경주, Time: 181.69 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
@@ -1,4 +1,17 @@
 function solution(players, callings) {
-    var answer = [];
-    return answer;
+  const playersObj = {};
+  for (let i = 0; i < players.length; i++) {
+    playersObj[players[i]] = i;
+  }
+
+  for (let i = 0; i < callings.length; i++) {
+    const idx = playersObj[callings[i]];
+    const temp = players[idx - 1];
+    playersObj[temp] = playersObj[temp] + 1;
+    playersObj[callings[i]] = playersObj[callings[i]] - 1;
+
+    players[idx - 1] = callings[i];
+    players[idx] = temp;
+  }
+  return players;
 }
