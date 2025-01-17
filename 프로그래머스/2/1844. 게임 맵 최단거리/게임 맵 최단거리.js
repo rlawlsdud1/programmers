6ad4a7b4b895f436b1488748ba4a43f9983ea1c6
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-01-17 21:45:57 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 20.86 ms, Memory: 38.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,7 +1,4 @@
 function solution(maps) {
-  const queue = [];
-  const n = maps.length;
-  const m = maps[0].length;
   const directions = [
     [1, 0],
     [-1, 0],
@@ -9,29 +6,32 @@ function solution(maps) {
     [0, -1],
   ];
 
+  const queue = [];
+  queue.push([0, 0, 1]); // 시작점 push
   maps[0][0] = 0;
-  queue.push([0, 0, 1]);
 
-  while (queue.length > 0) {
+  while (queue.length) {
     const [x, y, distance] = queue.shift();
 
-    // queue에서 꺼낸게 탈출구인지?
-    if (x === n - 1 && y === m - 1) {
+    if (x === maps.length - 1 && y === maps[0].length - 1) {
       return distance;
     }
 
-    // 4방향 탐색
-    for (let i = 0; i < 4; i++) {
+    // 4방향 탐색하면서 가능한 경로 queue에 push
+    for (let i = 0; i < directions.length; i++) {
       const [nx, ny] = [x + directions[i][0], y + directions[i][1]];
-
-      // 갈 수 있는 곳이면 방문처리하고 enqueue
-      if (nx >= 0 && ny >= 0 && nx < n && ny < m && maps[nx][ny] === 1) {
+      if (
+        nx >= 0 &&
+        ny >= 0 &&
+        nx < maps.length &&
+        ny < maps[0].length &&
+        maps[nx][ny] === 1
+      ) {
         maps[nx][ny] = 0;
         queue.push([nx, ny, distance + 1]);
       }
     }
   }
 
-  // while loop가 진행되는 동안 탈출구를 못찾았으면 -1 return
   return -1;
 }
