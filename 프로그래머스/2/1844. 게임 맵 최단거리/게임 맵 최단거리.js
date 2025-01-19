b4acc4983d8656bf30f8a709a0125ea4fdd7ae32
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-01-19 15:17:56 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 20.77 ms, Memory: 38.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,36 +1,40 @@
 function solution(maps) {
-  const n = maps.length;
-  const m = maps[0].length;
   const directions = [
     [1, 0],
     [-1, 0],
     [0, 1],
     [0, -1],
   ];
-
-  // maps를 직접 수정하면서 방문기록을 갱신
-  // 방문 배열 필요 X
-
   const queue = [];
-  queue.push([0, 0, 1]); // 좌표와 거리로 구성된 배열
+  queue.push([0, 0, 1]); // 시작좌표 및 거리
 
   while (queue.length) {
     const [x, y, distance] = queue.shift();
 
-    if (x === n - 1 && y === m - 1) {
+    if (x === maps.length - 1 && y === maps[0].length - 1) {
       return distance;
     }
 
     for (let i = 0; i < directions.length; i++) {
+      // 새롭게 갈 수 있는 좌표
       const [nx, ny] = [x + directions[i][0], y + directions[i][1]];
 
-      if (nx >= 0 && ny >= 0 && nx < n && ny < m && maps[nx][ny] === 1) {
-        queue.push([nx, ny, distance + 1]);
+      // 실제로 갈 수 있는지 체크
+      if (
+        nx >= 0 &&
+        ny >= 0 &&
+        nx < maps.length &&
+        ny < maps[0].length &&
+        maps[nx][ny] === 1
+      ) {
+        // 갈 수 있다면 방문처리하고, queue에 push
+        // distance는 기존 지점의 distance + 1
         maps[nx][ny] = 0;
+        queue.push([nx, ny, distance + 1]);
       }
     }
   }
 
-  // 반복문이 돌때까지 도착지점에 도달하지 못한다면
+  // 갈 수 있는 곳을 다 체크했는데 목표지점에 도달하지 못한다면
   return -1;
 }
