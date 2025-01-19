2ec39dccb017000c00f3a4784f47e5515fd04e1d
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-01-19 15:30:32 +0900

    [level 3] Title: 가장 먼 노드, Time: 79.66 ms, Memory: 55.9 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
@@ -1,35 +1,33 @@
 function solution(n, edge) {
   const adjacantList = {};
-  edge.forEach((e) => {
-    adjacantList[e[0]]
-      ? adjacantList[e[0]].push(e[1])
-      : (adjacantList[e[0]] = [e[1]]);
-    adjacantList[e[1]]
-      ? adjacantList[e[1]].push(e[0])
-      : (adjacantList[e[1]] = [e[0]]);
-  });
-
+  for (let i = 0; i < edge.length; i++) {
+    adjacantList[edge[i][0]]
+      ? adjacantList[edge[i][0]].push(edge[i][1])
+      : (adjacantList[edge[i][0]] = [edge[i][1]]);
+    adjacantList[edge[i][1]]
+      ? adjacantList[edge[i][1]].push(edge[i][0])
+      : (adjacantList[edge[i][1]] = [edge[i][0]]);
+  }
+  // 노드 번호와 인덱스 맞춰주기 위해 n+1 로 설정
   const visited = Array.from({ length: n + 1 }).fill(false);
   const distances = Array.from({ length: n + 1 }).fill(0);
-
   const queue = [];
-  queue.push(1); // 1번 노드부터 시작
+  queue.push([1, 0]); // 1번 노드와 거리 push
   visited[1] = true;
 
   while (queue.length) {
-    const node = queue.shift();
-    const adjacantNodes = adjacantList[node];
+    const [node, distance] = queue.shift();
 
-    for (let i = 0; i < adjacantNodes.length; i++) {
-      if (!visited[adjacantNodes[i]]) {
-        visited[adjacantNodes[i]] = true;
-        queue.push(adjacantNodes[i]);
-        distances[adjacantNodes[i]] = distances[node] + 1;
+    for (let i = 0; i < adjacantList[node].length; i++) {
+      const adjacantNode = adjacantList[node][i];
+
+      if (!visited[adjacantNode]) {
+        visited[adjacantNode] = true;
+        queue.push([adjacantNode, distance + 1]);
+        distances[adjacantNode] = distance + 1;
       }
     }
   }
-
   distances.sort((a, b) => a - b);
-
   return distances.filter((v) => v === distances.at(-1)).length;
 }
