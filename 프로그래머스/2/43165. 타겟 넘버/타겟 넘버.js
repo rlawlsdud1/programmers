e0d2faeb7bff059c5e7f899b6cb30a46e710c237
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-01-22 15:45:14 +0900

    [level 2] Title: 타겟 넘버, Time: 14.64 ms, Memory: 35.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,20 +1,19 @@
 function solution(numbers, target) {
   let answer = 0;
-  const length = numbers.length;
 
-  function dfs(count, sum) {
-    if (count === length) {
+  function DFS(sum, count) {
+    if (count === numbers.length) {
       if (sum === target) {
         answer++;
       }
       return;
     }
 
-    dfs(count + 1, sum + numbers[count]);
-    dfs(count + 1, sum - numbers[count]);
+    DFS(sum + numbers[count], count + 1);
+    DFS(sum - numbers[count], count + 1);
   }
 
-  dfs(0, 0);
+  DFS(0, 0);
 
   return answer;
 }
