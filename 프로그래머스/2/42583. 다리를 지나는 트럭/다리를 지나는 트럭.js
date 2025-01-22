66982ec11e84b496d1ed4b720c157053189f4c17
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-01-22 15:31:05 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 3193.53 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -1,29 +1,30 @@
 function solution(bridge_length, weight, truck_weights) {
   let count = 0;
-
-  // 다리 위에 있는 트럭들을 담은 배열
-  const onBridge = [];
-
-  // 대기 중인 트럭이 다 빠지고, 다리 위에 트럭이 없을때까지 반복
-  while (truck_weights.length || onBridge.filter((e) => e !== 0).length) {
-    // 다리 길이를 초과한 경우 다리에서 트럭 제거
-    if (onBridge.length >= bridge_length) {
-      onBridge.shift();
+  const n = truck_weights.length;
+  const bridgeArr = Array.from({ length: bridge_length }).fill(0);
+  const complete = [];
+  while (1) {
+    if (complete.length === n) {
+      break;
     }
-    // 다리 위에 있는 트럭들의 무게 합
-    const bridgeSum = onBridge.reduce((total, e) => total + e, 0);
-    if (
-      bridgeSum + truck_weights[0] <= weight &&
-      onBridge.length < bridge_length
-    ) {
-      // 트럭을 다리에 올림
-      count++;
-      onBridge.push(truck_weights.shift());
+
+    const outOfBridge = bridgeArr.shift();
+    if (outOfBridge) {
+      complete.push(outOfBridge);
+    }
+
+    if (truck_weights[0] + sumOfTrucks(bridgeArr) <= weight) {
+      bridgeArr.push(truck_weights.shift());
     } else {
-      // 트럭을 올릴 수 없으면 다리 길이를 유지하기 위해 0을 추가
-      count++;
-      onBridge.push(0);
+      bridgeArr.push(0);
     }
+
+    count++;
   }
+
   return count;
+}
+
+function sumOfTrucks(bridge) {
+  return bridge.reduce((acc, cur) => acc + cur, 0);
 }
