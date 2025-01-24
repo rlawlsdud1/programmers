d520bb99f967ec3d18f51ad40861f41896734535
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-01-24 16:22:15 +0900

    [level 2] Title: 구명보트, Time: 13.25 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/README.md
+++ 프로그래머스/2/42885. 구명보트/README.md
@@ -1,10 +1,10 @@
 # [level 2] 구명보트 - 42885 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885) 
 
 ### 성능 요약
 
-메모리: 37.7 MB, 시간: 33.41 ms
+메모리: 38.2 MB, 시간: 13.25 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 13일 16:19:31
+2025년 01월 24일 16:22:13
 
 ### 문제 설명
 
--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,15 +1,16 @@
 function solution(people, limit) {
   let answer = 0;
-  people = people.sort((a, b) => b - a);
-  let [left, right] = [0, people.length - 1];
+  people.sort((a, b) => a - b);
+  let left = 0;
+  let right = people.length - 1;
   while (left <= right) {
-    if (people[left] + people[right] > limit) {
-      left++;
+    if (people[left] + people[right] <= limit) {
       answer++;
-    } else {
       left++;
       right--;
+    } else {
       answer++;
+      right--;
     }
   }
   return answer;
