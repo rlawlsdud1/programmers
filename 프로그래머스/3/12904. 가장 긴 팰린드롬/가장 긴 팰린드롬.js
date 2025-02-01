fef63c9f2028226f5f4696e413b1ff9c99cde34d
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-02-01 16:48:36 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 17.50 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.js
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.js
@@ -2,15 +2,15 @@ function solution(s) {
   let answer = 1;
 
   // 짝수일 때
-  for (let i = 0; i < s.length; i++) {
+  for (let i = 0; i < s.length - 1; i++) {
     let left = i;
     let right = i + 1;
 
     let front = s.slice(left, i + 1);
     let back = s.slice(i + 1, right + 1);
 
-    while (left >= 0 && right < s.length && checkSymmetry(front, back)) {
-      answer = Math.max(answer, 2 * front.length);
+    while (left >= 0 && right < s.length && checkPalindrome(front, back)) {
+      answer = Math.max(answer, front.length * 2);
 
       front = s.slice(--left, i + 1);
       back = s.slice(i + 1, ++right + 1);
@@ -25,8 +25,8 @@ function solution(s) {
     let front = s.slice(left, i);
     let back = s.slice(i + 1, right + 1);
 
-    while (left >= 0 && right < s.length && checkSymmetry(front, back)) {
-      answer = Math.max(answer, 2 * front.length + 1);
+    while (left >= 0 && right < s.length && checkPalindrome(front, back)) {
+      answer = Math.max(answer, front.length * 2 + 1);
 
       front = s.slice(--left, i);
       back = s.slice(i + 1, ++right + 1);
@@ -36,6 +36,14 @@ function solution(s) {
   return answer;
 }
 
-function checkSymmetry(str1, str2) {
-  return str1 === str2.split("").reverse().join("");
+function checkPalindrome(front, back) {
+  back = back.split("").reverse();
+  front = front.split("");
+
+  for (let i = 0; i < front.length; i++) {
+    if (front[i] !== back[i]) {
+      return false;
+    }
+  }
+  return true;
 }
