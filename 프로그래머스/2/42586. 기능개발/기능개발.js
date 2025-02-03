b6ec9e17c5b0bfc785464ab5c778c03f13e9ebac
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-02-03 21:22:58 +0900

    [level 2] Title: 기능개발, Time: 0.51 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,19 +1,25 @@
 function solution(progresses, speeds) {
-  let remainProgresses = [];
-  for (let i = 0; i < progresses.length; i++) {
-    const remain = 100 - progresses[i];
-    remainProgresses.push(Math.ceil(remain / speeds[i]));
-  }
-
+  // 1. progresses[i] 에 speeds[i] 를 계속 더한다
+  // 2. progresses[0] 이 100 넘는지 확인한다
+  // 3. 그렇다면 shift, 그 이후것도 100 넘는지 확인
+  // 4. 반복하고 안넘는다면 다시 1번 반복
   const answer = [];
-  while (remainProgresses.length) {
-    let i = remainProgresses.findIndex((e) => e > remainProgresses[0]);
-    if (i === -1) {
-      i = remainProgresses.length;
-    }
-    remainProgresses = remainProgresses.slice(i);
-    answer.push(i);
-  }
 
+  while (progresses.length) {
+    for (let i = 0; i < progresses.length; i++) {
+      progresses[i] += speeds[i];
+    }
+
+    let count = 0;
+    while (progresses[0] >= 100) {
+      progresses.shift();
+      speeds.shift();
+      count++;
+    }
+
+    if (count) {
+      answer.push(count);
+    }
+  }
   return answer;
 }
