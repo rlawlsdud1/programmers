0d1cf3d44a1aceed6b4733ae7bc2a469143dbba5
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-02-04 14:12:04 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 5803.39 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -1,30 +1,26 @@
 function solution(bridge_length, weight, truck_weights) {
   let count = 0;
-  const n = truck_weights.length;
-  const bridgeArr = Array.from({ length: bridge_length }).fill(0);
-  const complete = [];
+  const bridge = Array.from({ length: bridge_length }).fill(0);
+
   while (1) {
-    if (complete.length === n) {
+    // 종료 조건
+    if (count > 1 && bridge.reduce((acc, cur) => acc + cur, 0) === 0) {
       break;
     }
 
-    const outOfBridge = bridgeArr.shift();
-    if (outOfBridge) {
-      complete.push(outOfBridge);
+    bridge.shift();
+    if (truck_weights.length) {
+      if (
+        bridge.reduce((acc, cur) => acc + cur, 0) + truck_weights[0] <=
+        weight
+      ) {
+        bridge.push(truck_weights.shift());
+      } else {
+        bridge.push(0);
+      }
     }
-
-    if (truck_weights[0] + sumOfTrucks(bridgeArr) <= weight) {
-      bridgeArr.push(truck_weights.shift());
-    } else {
-      bridgeArr.push(0);
-    }
-
     count++;
   }
 
   return count;
-}
-
-function sumOfTrucks(bridge) {
-  return bridge.reduce((acc, cur) => acc + cur, 0);
 }
