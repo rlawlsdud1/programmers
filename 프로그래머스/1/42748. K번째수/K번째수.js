87126223454d530e51cd9c59a3331d95ec0d885a
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-02-06 19:59:09 +0900

    [level 1] Title: K번째수, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -0,0 +1,8 @@
+function solution(array, commands) {
+  const answer = [];
+  commands.forEach((v) => {
+    answer.push(array.slice(v[0] - 1, v[1]).sort((a, b) => a - b)[v[2] - 1]);
+  });
+
+  return answer;
+}
