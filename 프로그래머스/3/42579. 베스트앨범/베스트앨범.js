76cf51630c65befb5062960ef9048f547e0a2345
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-02-06 18:34:09 +0900

    [level 3] Title: 베스트앨범, Time: 0.33 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -1,44 +1,28 @@
 function solution(genres, plays) {
+  const musicObj = {};
+  const playCountObj = {};
+  genres.forEach((v, i) => {
+    musicObj[v]
+      ? musicObj[v].push([plays[i], i])
+      : (musicObj[v] = [[plays[i], i]]);
+    playCountObj[v]
+      ? (playCountObj[v] += plays[i])
+      : (playCountObj[v] = plays[i]);
+  });
+  const playCountArr = Object.entries(playCountObj);
+  playCountArr.sort((a, b) => b[1] - a[1]);
+
   const answer = [];
-  const hashedGenres = {};
-  const genreForPlay = {};
-  for (let i = 0; i < genres.length; i++) {
-    if (hashedGenres[genres[i]]) {
-      hashedGenres[genres[i]] += plays[i];
-      genreForPlay[genres[i]].push([i, plays[i]]);
-      genreForPlay[genres[i]].sort((a, b) => {
-        // 두 번째 원소를 기준으로 비교
-        if (a[1] === b[1]) {
-          // 같으면 첫 번째 원소로 비교
-          return a[0] - b[0];
-        }
-        return b[1] - a[1];
-      });
+  playCountArr.forEach((v) => {
+    const [genre, _] = v;
+    musicObj[genre].sort((a, b) => b[0] - a[0]);
+    if (musicObj[genre].length > 1) {
+      answer.push(musicObj[genre][0][1]);
+      answer.push(musicObj[genre][1][1]);
     } else {
-      hashedGenres[genres[i]] = plays[i];
-      genreForPlay[genres[i]] = [[i, plays[i]]];
-      genreForPlay[genres[i]].sort((a, b) => {
-        if (a[1] === b[1]) {
-          return a[0] - b[0];
-        }
-        return b[1] - a[1];
-      });
+      answer.push(musicObj[genre][0][1]);
     }
-  }
-
-  // hashedGenres의 [key, value]가 담긴 배열
-  const keysFromHashedGenres = Object.entries(hashedGenres);
-  // keysFromHashedGenres의 각 원소에서 두번째 원소를 기준으로 정렬한 배열
-  const sortedArr = keysFromHashedGenres.sort((a, b) => b[1] - a[1]);
-
-  // 여기서 index에러가 나는 듯 하다.
-  for (let i = 0; i < sortedArr.length; i++) {
-    const genreKey = sortedArr[i][0];
-    answer.push(genreForPlay[genreKey][0][0]);
-    if (genreForPlay[genreKey][1]) {
-      answer.push(genreForPlay[genreKey][1][0]);
-    }
-  }
+  });
 
   return answer;
 }
