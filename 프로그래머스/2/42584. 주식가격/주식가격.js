bd547df4fb9080f0e29be3cd62d181488b2b956e
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-02-07 11:38:43 +0900

    [level 2] Title: 주식가격, Time: 47.55 ms, Memory: 43.3 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,23 +1,16 @@
 function solution(prices) {
   const answer = Array.from({ length: prices.length }).fill(0);
   const stack = [];
-
   for (let i = 0; i < prices.length; i++) {
     while (stack.length && prices[stack.at(-1)] > prices[i]) {
       const j = stack.pop();
       answer[j] = i - j;
     }
-
     stack.push(i);
   }
-  // stack에 있는 원소는 끝내 안떨어진 주식의 index
-
-  while (stack.length) {
-    const j = stack.pop();
-    // prices 의 lastIndex - j 는 j 번째 주식이 안떨어진 기간(초)
-    answer[j] = prices.length - 1 - j;
-  }
+  stack.forEach((v) => {
+    answer[v] = prices.length - 1 - v;
+  });
 
   return answer;
-    
 }
