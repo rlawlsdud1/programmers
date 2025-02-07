f43358fc6d93f5b43ccfcf738738c9cee24bbaf5
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-02-07 20:06:02 +0900

    [level 2] Title: 주식가격, Time: 39.58 ms, Memory: 41.6 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,16 +1,16 @@
 function solution(prices) {
-  const answer = Array.from({ length: prices.length }).fill(0);
-  const stack = [];
-  for (let i = 0; i < prices.length; i++) {
-    while (stack.length && prices[stack.at(-1)] > prices[i]) {
-      const j = stack.pop();
-      answer[j] = i - j;
+    // loop 2개 돌리면 당연히 답은 나오겠지만, 당연히 시간초과 뜬다.
+    const answer = Array.from({length:prices.length}).fill(0)
+    const stack = []
+    for(let i=0; i<prices.length; i++){
+        while(stack.length && prices[stack.at(-1)] > prices[i]){
+            const j = stack.pop()
+            answer[j] = i - j
+        }
+        stack.push(i)
     }
-    stack.push(i);
-  }
-  stack.forEach((v) => {
-    answer[v] = prices.length - 1 - v;
-  });
-
-  return answer;
+    stack.forEach((v) => {
+        answer[v] = prices.length - 1 - v
+    })
+    return answer;
 }
