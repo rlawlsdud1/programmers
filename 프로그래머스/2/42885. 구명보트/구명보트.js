1f62fb6b2a2214e07ee08feca7f1fed407818dea
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-02-07 12:35:15 +0900

    [level 2] Title: 구명보트, Time: 13.72 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,18 +1,19 @@
 function solution(people, limit) {
-  let answer = 0;
   people.sort((a, b) => a - b);
-  let left = 0;
-  let right = people.length - 1;
+
+  let left = 0,
+    right = people.length - 1,
+    count = 0;
 
   while (left <= right) {
     if (people[left] + people[right] <= limit) {
       left++;
       right--;
-      answer++;
     } else {
       right--;
-      answer++;
     }
+    count++;
   }
-  return answer;
+
+  return count;
 }
