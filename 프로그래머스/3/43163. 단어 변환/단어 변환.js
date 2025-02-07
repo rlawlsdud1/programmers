65fa3b2c216bcc7bfd3ef7a76399914bb09b2831
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-02-07 17:40:33 +0900

    [level 3] Title: 단어 변환, Time: 0.41 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -1,40 +1,29 @@
 function solution(begin, target, words) {
-  let answer = Infinity;
-
   const visited = Array.from({ length: words.length }).fill(false);
+  let answer = Infinity;
   function DFS(word, count) {
     if (word === target) {
       answer = Math.min(answer, count);
-      return;
     }
     for (let i = 0; i < words.length; i++) {
-      if (!visited[i] && canChange(word, words[i])) {
+      if (!visited[i] && checkCanChange(word, words[i])) {
         visited[i] = true;
-
         DFS(words[i], count + 1);
+
         visited[i] = false;
       }
     }
   }
-
   DFS(begin, 0);
-
-  if (answer === Infinity) {
-    return 0;
-  }
-
-  return answer;
+  if (answer !== Infinity) return answer;
+  return 0;
 }
 
-function canChange(str1, str2) {
+function checkCanChange(str1, str2) {
   let count = 0;
   for (let i = 0; i < str1.length; i++) {
-    if (str1[i] !== str2[i]) {
-      count++;
-    }
-    if (count > 1) {
-      return false;
-    }
+    if (str1[i] !== str2[i]) count++;
   }
-  return true;
+  if (count === 1) return true;
+  return false;
 }
