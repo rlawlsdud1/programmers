57b0f36ccc5997c73e86652f3dd97d39e773362f
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-02-11 17:20:56 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 20.79 ms, Memory: 38.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,13 +1,18 @@
 function solution(maps) {
+  // n x m 형태의 map
+  // 필수적으로
   const directions = [
     [1, 0],
     [-1, 0],
     [0, 1],
     [0, -1],
   ];
+
   const queue = [];
   queue.push([0, 0, 1]);
+  maps[0][0] = 0;
 
+  // 하나씩 꺼낼거임
   while (queue.length) {
     const [x, y, distance] = queue.shift();
 
@@ -18,6 +23,7 @@ function solution(maps) {
     for (let i = 0; i < directions.length; i++) {
       const [nx, ny] = [x + directions[i][0], y + directions[i][1]];
 
+      // 실제로 갈 수 있는지를 체크할거임
       if (
         nx >= 0 &&
         ny >= 0 &&
@@ -25,13 +31,13 @@ function solution(maps) {
         ny < maps[0].length &&
         maps[nx][ny] === 1
       ) {
-        maps[nx][ny] = 0;
+        // 실제로 갈 수 있다면 다시 queue에 넣어줄거임
         queue.push([nx, ny, distance + 1]);
+        // maps 를 바꿔줄거임.
+        maps[nx][ny] = 0;
       }
     }
-  }
+  }//
 
   return -1;
 }
-
-// 최단거리
