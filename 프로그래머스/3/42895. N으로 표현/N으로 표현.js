d9e8d20746484e7801dbec2e8bd1715b35992da5
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-02-19 14:52:07 +0900

    [level 3] Title: N으로 표현, Time: 36.34 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.js
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.js
@@ -1,14 +1,12 @@
 function solution(N, number) {
-  // index 맞추기 위해 length 9로 설정
   const dp = Array.from({ length: 9 }, () => new Set());
 
-  // N을 1 ~ 9개 사용하여 만들 수 있는 수들을 만들 것이다
-  for (let i = 1; i < 9; i++) {
+  for (let i = 1; i <= 8; i++) {
     dp[i].add(Number(String(N).repeat(i)));
 
     for (let j = 1; j < i; j++) {
-      for (let a of dp[j]) {
-        for (let b of dp[i - j]) {
+      for (const a of dp[j]) {
+        for (const b of dp[i - j]) {
           dp[i].add(a * b);
           dp[i].add(a + b);
           dp[i].add(a - b);
