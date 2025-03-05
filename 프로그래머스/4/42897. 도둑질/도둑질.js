f5d539061f7b50d6b9227172e80995bad247047f
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-03-05 12:54:39 +0900

    [level 4] Title: 도둑질, Time: 142.59 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.js
+++ 프로그래머스/4/42897. 도둑질/도둑질.js
@@ -1,33 +1,28 @@
 function solution(money) {
-  // 집들이 원형으로 배치되어 있기 때문에,
-  // 첫 번째 집과 마지막 집도 서로 인접한 것으로 간주
+  // 집들이 원형으로 위치하기에 처음과 끝이 연결되어 있다.
+  // 따라서 첫번째 집을 터는 경우의 dp1, 안 터는 경우의 dp2 를 만들고
+  // 마지막 원소가 더 큰 것을 return 한다.
+  // dp[i] 가 의미하는 것은 i번째 집까지 왔을 때의 최댓값을 뜻한다.
+  const n = money.length;
+  const dp1 = Array.from({ length: n }).fill(0);
+  const dp2 = Array.from({ length: n }).fill(0);
 
-  // dp1 은 첫번째 집을 털었을 때의 dp table
-  //  -> 첫번째 집을 턴다면, 마지막 집을 털 수 없음
-
-  // dp2 는 첫번째 집을 안 털었을 때의 dp table
-  //  -> 첫번째 집을 안 턴다면, 마지막 집을 털 수 있음
-
-  // 위 사항을 고려하여 loop range setting
-
-  // dp[i] 는 i번째집까지 왔을 때의 최댓값
-
-  const dp1 = Array.from({ length: money.length });
+  // 첫번째 집을 터는 경우의 초기값 세팅
   dp1[0] = money[0];
   dp1[1] = Math.max(money[0], money[1]);
 
-  const dp2 = Array.from({ length: money.length });
+  // 첫번째 집을 안터는 경우의 초기값 세팅
   dp2[0] = 0;
   dp2[1] = money[1];
 
-  for (let i = 2; i < money.length - 1; i++) {
+  // 첫번째 집을 턴다면 마지막 집은 못턴다다
+  for (let i = 2; i < n - 1; i++) {
     dp1[i] = Math.max(dp1[i - 1], dp1[i - 2] + money[i]);
   }
 
-  for (let i = 2; i < money.length; i++) {
+  for (let i = 2; i < n; i++) {
     dp2[i] = Math.max(dp2[i - 1], dp2[i - 2] + money[i]);
   }
 
-  // 여기서도 index 조심.
-  return Math.max(dp1.at(-2), dp2.at(-1));
+  return Math.max(dp1[n - 2], dp2[n - 1]);
 }
