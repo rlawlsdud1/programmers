662ad77f279ccf433d4e8c306158bd68e84f21a4
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-03-10 14:30:16 +0900

    [Gold V] Title: 평범한 배낭, Time: 960 ms, Memory: 92132 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.js
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.js
@@ -1,30 +1,33 @@
 const fs = require("fs");
-// 제출할때는 '/dev/stdin' 로 바꾸기
-let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+const path = require("path");
+const filePath =
+  process.platform === "linux"
+    ? "/dev/stdin"
+    : path.join(__dirname, "../input.txt");
+let input = fs
+  .readFileSync(filePath)
+  .toString()
+  .split("\n")
+  .map((v) => v.trim());
 
-input = input.map((v) => v.trim()).map((e) => e.split(" ").map(Number));
-const [N, K] = input[0];
-const info = input.slice(1);
-
-// dp[i][w] 는 i번째 물건에서 w 이하의 무게들을 담을 때의 최댓값
-// 이를 점화식으로 나타내면
-// dp[i][w] = Math.max( dp[i-1][w], dp[i-1][w-w[i]] + v[i] )
-// 이를 해석해보면 i번째를 안담았을 때, i번째를 담았을 때 중 큰 값을 선택
-// 근데 i번째를 담는다면 w-w[i] 는 0 이상이어야 한다.
+const [N, K] = input[0].split(" ").map(Number);
+const info = input.slice(1).map((v) => v.split(" ").map(Number));
 
 const dp = Array.from({ length: N + 1 }, () =>
   Array.from({ length: K + 1 }).fill(0)
 );
 
 for (let i = 1; i <= N; i++) {
-  const thing = info[i - 1];
+  const currentObject = info[i - 1];
   for (let w = 1; w <= K; w++) {
-    if (w - thing[0] >= 0) {
-      dp[i][w] = Math.max(dp[i - 1][w], dp[i - 1][w - thing[0]] + thing[1]);
+    if (w - currentObject[0] >= 0) {
+      dp[i][w] = Math.max(
+        dp[i - 1][w],
+        dp[i - 1][w - currentObject[0]] + currentObject[1]
+      );
     } else {
       dp[i][w] = dp[i - 1][w];
     }
   }
 }
-
 console.log(Math.max(...dp[N]));
