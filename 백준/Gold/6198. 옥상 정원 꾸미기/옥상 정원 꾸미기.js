7db40a34b315ac33a027d258bfc4638390a80780
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-03-12 20:14:52 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 196 ms, Memory: 24396 KB -BaekjoonHub

--- 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.js
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.js
@@ -10,24 +10,18 @@ let input = fs
   .split("\n")
   .map((v) => v.trim());
 
-// 자신이 위치한 빌딩보다 높거나 같은 빌딩이 있으면
-// 그 다음에 있는 모든 빌딩의 옥상은 보지 못한다.
 const N = Number(input[0]);
 const heights = input.slice(1).map((v) => Number(v));
 
-// 마지막에 있는 관리인은 어차피 오른쪽에 빌딩이 없어서 못봄
-// loop range setting
 let answer = 0;
-for (let i = 0; i < N - 1; i++) {
-  const cur = heights[i];
-  let count = 0;
-  for (let j = i + 1; j < N; j++) {
-    if (cur > heights[j]) {
-      count++;
-    } else {
-      break;
-    }
+const stack = [];
+
+for (let i = 0; i < N; i++) {
+  while (stack.length > 0 && stack[stack.length - 1] <= heights[i]) {
+    stack.pop();
   }
-  answer += count;
+  stack.push(heights[i]);
+  answer += stack.length - 1;
 }
+
 console.log(answer);
