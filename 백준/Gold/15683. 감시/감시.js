682a74c18c4b171ed0bcfd9f9f051a33623d517c
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-03-13 16:54:55 +0900

    [Gold III] Title: 감시, Time: 980 ms, Memory: 15748 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.js
+++ 백준/Gold/15683. 감시/감시.js
@@ -4,16 +4,14 @@ const filePath =
   process.platform === "linux"
     ? "/dev/stdin"
     : path.join(__dirname, "../input.txt");
-let input = fs.readFileSync(filePath).toString().split("\n");
+let input = fs
+  .readFileSync(filePath)
+  .toString()
+  .split("\n")
+  .map((v) => v.trim());
 
-// 0은 빈 칸, 6은 벽
-// 1~5는 cctv 번호
-// cctv는 cctv를 통과할 수 있다
-// cctv방향을 적절히 정해서 사각 지대의 최소 크기를 구하자
-// = 감시범위가 최댓값이어야 한다
-const [N, M] = input[0].trim().split(" ").map(Number);
-const map = input.slice(1).map((v) => v.trim().split(" ").map(Number));
-
+const [N, M] = input[0].split(" ").map(Number);
+const map = input.slice(1).map((v) => v.split(" ").map(Number));
 const directions = [
   [-1, 0],
   [0, 1],
@@ -21,15 +19,8 @@ const directions = [
   [0, -1],
 ];
 
-const cctvCoordinates = [];
-for (let i = 0; i < N; i++) {
-  for (let j = 0; j < M; j++) {
-    if ([1, 2, 3, 4, 5].includes(map[i][j])) {
-      cctvCoordinates.push([i, j, map[i][j]]);
-    }
-  }
-}
-
+// cctv 별 가능한 방향
+// 각 원소에 담긴 value들은 directions의 index와 같다
 const cctvDirections = {
   1: [[0], [1], [2], [3]],
   2: [
@@ -51,7 +42,17 @@ const cctvDirections = {
   5: [[0, 1, 2, 3]],
 };
 
+const cctvs = [];
+for (let i = 0; i < N; i++) {
+  for (let j = 0; j < M; j++) {
+    if ([1, 2, 3, 4, 5].includes(map[i][j])) {
+      cctvs.push([i, j, map[i][j]]);
+    }
+  }
+}
+
 function watch(x, y, dir, map) {
+  // cctv의 좌표를 인자로 받고 map을 갱신하는 함수
   let [dx, dy] = directions[dir];
   let nx = x + dx;
   let ny = y + dy;
@@ -63,33 +64,39 @@ function watch(x, y, dir, map) {
   }
 }
 
-let minValue = Infinity;
-function DFS(index, copiedMap) {
-  // 한 방향 탐색 하면, 다음 cctv 탐색
-  // index도 함께 전달해서 index가 cctv개수가 되면 return
-  // return하면 탐색했던 것 원복하고, 방향 바꿔서 다시 탐색
-  if (index === cctvCoordinates.length) {
-    // 여기서 사각지대 개수 갱신(최솟값으로)
-    let temp = 0;
+let answer = Infinity;
+
+function DFS(idx, updatedMap) {
+  if (idx === cctvs.length) {
+    // base case
+    // 사각지대 개수 갱신
+    let count = 0;
     for (let i = 0; i < N; i++) {
       for (let j = 0; j < M; j++) {
-        if (copiedMap[i][j] === 0) temp++;
+        if (updatedMap[i][j] === 0) count++;
       }
     }
-    minValue = Math.min(minValue, temp);
+    answer = Math.min(answer, count);
     return;
   }
-  const [x, y, type] = cctvCoordinates[index];
-  const originalMap = JSON.parse(JSON.stringify(copiedMap));
+  // 여기에 DFS 로직 작성
+  const [x, y, type] = cctvs[idx];
+  const copiedMap = JSON.parse(JSON.stringify(updatedMap));
 
+  // 가능한 방향에 대해
   for (const dirs of cctvDirections[type]) {
     for (const dir of dirs) {
-      watch(x, y, dir, copiedMap);
+      // 한 방향씩 뚫는다
+      watch(x, y, dir, updatedMap);
     }
-    DFS(index + 1, copiedMap);
-    copiedMap = JSON.parse(JSON.stringify(originalMap));
+    // type에 따라 가능한 방향을 탐색 했으면, 다음 cctv 탐색 출발
+    // (1번 cctv에 대해서는 위쪽 훑고 다음 탐색 진행하겠지)
+    DFS(idx + 1, updatedMap);
+
+    // 탐색 마치고 돌아오면 맵 원복 시키기
+    updatedMap = JSON.parse(JSON.stringify(copiedMap));
   }
 }
 
 DFS(0, map);
-console.log(minValue);
+console.log(answer);
