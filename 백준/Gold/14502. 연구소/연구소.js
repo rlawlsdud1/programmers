1ab9e2e2d78ff280fc6038475f443e8aa7868ccb
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-03-17 00:17:12 +0900

    [Gold IV] Title: 연구소, Time: 2340 ms, Memory: 13480 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.js
+++ 백준/Gold/14502. 연구소/연구소.js
@@ -4,66 +4,63 @@ const filePath =
   process.platform === "linux"
     ? "/dev/stdin"
     : path.join(__dirname, "../input.txt");
-let input = fs.readFileSync(filePath).toString().split("\n");
+const input = fs
+  .readFileSync(filePath)
+  .toString()
+  .split("\n")
+  .map((v) => v.trim());
 
-const [N, M] = input[0].trim().split(" ").map(Number);
-const map = input.slice(1).map((v) => v.trim().split(" ").map(Number));
-
-// 3 ≤ N, M ≤ 8
+const [N, M] = input[0].split(" ").map(Number);
+const map = input.slice(1, 1 + N).map((v) => v.split(" ").map(Number));
 const directions = [
   [1, 0],
   [-1, 0],
   [0, 1],
   [0, -1],
 ];
-
-function DFS(x, y, copy) {
-  for (let i = 0; i < 4; i++) {
-    const [nx, ny] = [x + directions[i][0], y + directions[i][1]];
-    if (nx >= 0 && ny >= 0 && nx < N && ny < M && !copy[nx][ny]) {
-      copy[nx][ny] = 2;
-      DFS(nx, ny, copy);
-    }
-  }
-}
-function countSafeArea(map) {
-  let count = 0;
-  for (let i = 0; i < N; i++) {
-    for (let j = 0; j < M; j++) {
-      if (!map[i][j]) count++;
-    }
-  }
-  return count;
-}
 let answer = 0;
-// 꼭 3개를 세워야 한다
-function installWalls(count) {
+function BT(count) {
   if (count === 3) {
-    const copy = JSON.parse(JSON.stringify(map));
-
+    const copiedMap = JSON.parse(JSON.stringify(map));
     for (let i = 0; i < N; i++) {
       for (let j = 0; j < M; j++) {
-        if (copy[i][j] === 2) {
-          DFS(i, j, copy);
+        if (copiedMap[i][j] === 2) {
+          DFS(i, j, copiedMap);
         }
       }
     }
-    const count = countSafeArea(copy);
+    let count = 0;
+    for (let i = 0; i < N; i++) {
+      for (let j = 0; j < M; j++) {
+        if (copiedMap[i][j] === 0) {
+          count++;
+        }
+      }
+    }
     answer = Math.max(answer, count);
     return;
   }
-
   for (let i = 0; i < N; i++) {
     for (let j = 0; j < M; j++) {
-      if (!map[i][j]) {
+      if (map[i][j] === 0) {
         map[i][j] = 1;
-        installWalls(count + 1);
+        BT(count + 1);
         map[i][j] = 0;
       }
     }
   }
 }
 
-installWalls(0);
+function DFS(x, y, map) {
+  map[x][y] = 2;
 
+  for (let i = 0; i < 4; i++) {
+    const [nx, ny] = [x + directions[i][0], y + directions[i][1]];
+    if (nx >= 0 && ny >= 0 && nx < N && ny < M && map[nx][ny] === 0) {
+      DFS(nx, ny, map);
+    }
+  }
+}
+
+BT(0);
 console.log(answer);
