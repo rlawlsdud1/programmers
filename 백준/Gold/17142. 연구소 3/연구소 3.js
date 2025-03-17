6b6b18ca7696a38523e85574f59c8b7f998c3d42
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-03-17 18:53:23 +0900

    [Gold III] Title: 연구소 3, Time: 348 ms, Memory: 15144 KB -BaekjoonHub

--- 백준/Gold/17142. 연구소 3/연구소 3.js
+++ 백준/Gold/17142. 연구소 3/연구소 3.js
@@ -13,75 +13,84 @@ const input = fs
 const [N, M] = input[0].split(" ").map(Number);
 const map = input.slice(1, 1 + N).map((v) => v.split(" ").map(Number));
 
-const virusPositions = [];
-let safeZoneCnt = 0;
+// 백트래킹으로 활성 바이러스 M개 되면 bfs시작
+// bfs를 기존 패턴으로 쓰면 안된다.
+// 동시에 퍼지고, 시간초를 기록해야 하기 때문에.
+// 또한 매 반복마다 map에 0이 남아있는지를 기록하기보다는
+// 0의 개수를 미리 구해놓고, 깎는 방식으로 해야한다
+
+let safeZoneCount = 0;
+const viruses = [];
 for (let i = 0; i < N; i++) {
   for (let j = 0; j < N; j++) {
-    if (map[i][j] === 2) virusPositions.push([i, j]);
-    else if (map[i][j] === 0) safeZoneCnt++;
+    if (map[i][j] === 2) viruses.push([i, j]);
+    else if (map[i][j] === 0) safeZoneCount++;
   }
 }
 
-function BFS(selectedViruses) {
+function BFS(activeViruses) {
   const directions = [
     [1, 0],
     [-1, 0],
     [0, 1],
     [0, -1],
   ];
+  const queue = [];
   const visited = Array.from({ length: N }, () =>
     Array.from({ length: N }).fill(false)
   );
-  const queue = [];
-  let safeZoneCount = safeZoneCnt;
-  let time = 0;
 
-  selectedViruses.forEach(([x, y]) => {
-    queue.push([x, y]);
+  activeViruses.forEach((v) => {
+    const [x, y] = v;
     visited[x][y] = true;
+    queue.push([x, y]);
   });
 
-  while (queue.length > 0) {
-    if (safeZoneCount === 0) return time;
+  let count = 0; // 시간초를 담은 변수
+  let safeAreaCount = safeZoneCount; // loop를 돌며 얘를 깎을거임. 0이 되면 다 퍼진 상황
 
-    const lengthOfQueue = queue.length;
+  while (queue.length) {
+    if (safeAreaCount === 0) return count;
+    const queueLength = queue.length;
 
-    for (let i = 0; i < lengthOfQueue; i++) {
+    // queue 길이만큼 반복. 한번에 퍼트리는 로직
+    for (let i = 0; i < queueLength; i++) {
       const [x, y] = queue.shift();
-      for (const [dx, dy] of directions) {
-        const nx = x + dx;
-        const ny = y + dy;
 
-        if (nx >= 0 && nx < N && ny >= 0 && ny < N) {
-          if (!visited[nx][ny] && map[nx][ny] !== 1) {
+      for (let j = 0; j < 4; j++) {
+        const [nx, ny] = [x + directions[j][0], y + directions[j][1]];
+        if (nx >= 0 && ny >= 0 && nx < N && ny < N) {
+          if (map[nx][ny] !== 1 && !visited[nx][ny]) {
+            if (map[nx][ny] === 0) safeAreaCount--;
+
             visited[nx][ny] = true;
             queue.push([nx, ny]);
-
-            if (map[nx][ny] === 0) safeZoneCount--;
           }
         }
       }
     }
-    time++;
+
+    // 한번 퍼트렸으면 count 1증가
+    count++;
   }
 
   return Infinity;
 }
 
 let answer = Infinity;
-function backtrack(start, path) {
+function BT(start, path) {
   if (path.length === M) {
-    const time = BFS(path);
-    answer = Math.min(answer, time);
+    const count = BFS(path);
+    answer = Math.min(answer, count);
     return;
   }
 
-  for (let i = start; i < virusPositions.length; i++) {
-    path.push(virusPositions[i]);
-    backtrack(i + 1, path);
+  for (let i = start; i < viruses.length; i++) {
+    path.push(viruses[i]);
+    BT(i + 1, path);
     path.pop();
   }
 }
 
-backtrack(0, []);
+BT(0, []);
 console.log(answer === Infinity ? -1 : answer);
