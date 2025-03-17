d61b8a74cfe369ec02ef37027bced77ce61db050
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-03-17 19:41:43 +0900

    [Gold IV] Title: 연구소, Time: 568 ms, Memory: 13856 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.js
+++ 백준/Gold/14502. 연구소/연구소.js
@@ -12,16 +12,29 @@ const input = fs
 
 const [N, M] = input[0].split(" ").map(Number);
 const map = input.slice(1, 1 + N).map((v) => v.split(" ").map(Number));
+const walls = [];
 const directions = [
   [1, 0],
   [-1, 0],
   [0, 1],
   [0, -1],
 ];
+
+for (let i = 0; i < N; i++) {
+  for (let j = 0; j < M; j++) {
+    if (map[i][j] === 0) walls.push([i, j]);
+  }
+}
+
 let answer = 0;
-function BT(count) {
-  if (count === 3) {
+
+function BT(start, path) {
+  if (path.length === 3) {
     const copiedMap = JSON.parse(JSON.stringify(map));
+    path.forEach((v) => {
+      const [x, y] = v;
+      copiedMap[x][y] = 1;
+    });
     for (let i = 0; i < N; i++) {
       for (let j = 0; j < M; j++) {
         if (copiedMap[i][j] === 2) {
@@ -29,38 +42,34 @@ function BT(count) {
         }
       }
     }
+
     let count = 0;
     for (let i = 0; i < N; i++) {
       for (let j = 0; j < M; j++) {
-        if (copiedMap[i][j] === 0) {
-          count++;
-        }
+        if (copiedMap[i][j] === 0) count++;
       }
     }
+
     answer = Math.max(answer, count);
     return;
   }
-  for (let i = 0; i < N; i++) {
-    for (let j = 0; j < M; j++) {
-      if (map[i][j] === 0) {
-        map[i][j] = 1;
-        BT(count + 1);
-        map[i][j] = 0;
-      }
-    }
+
+  for (let i = start; i < walls.length; i++) {
+    path.push(walls[i]);
+    BT(i + 1, path);
+    path.pop();
   }
 }
 
 function DFS(x, y, map) {
-  map[x][y] = 2;
-
   for (let i = 0; i < 4; i++) {
     const [nx, ny] = [x + directions[i][0], y + directions[i][1]];
     if (nx >= 0 && ny >= 0 && nx < N && ny < M && map[nx][ny] === 0) {
+      map[nx][ny] = 2;
       DFS(nx, ny, map);
     }
   }
 }
 
-BT(0);
+BT(0, []);
 console.log(answer);
