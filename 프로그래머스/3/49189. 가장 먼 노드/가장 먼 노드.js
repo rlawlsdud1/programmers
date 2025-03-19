3325111eb94869e6d75672d56d45d083c3852425
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-03-19 23:41:22 +0900

    [level 3] Title: 가장 먼 노드, Time: 45.80 ms, Memory: 56.2 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
@@ -1,31 +1,29 @@
 function solution(n, edge) {
-  const adjacantList = Array.from({ length: n + 1 }, () => []);
-  edge.forEach((v) => {
-    const [from, to] = v;
-    adjacantList[from].push(to);
-    adjacantList[to].push(from);
-  });
-
-  const visited = Array.from({ length: n + 1 }).fill(false);
-  const distances = Array.from({ length: n + 1 }).fill(0);
-  const queue = [];
-  visited[1] = true;
-  distances[1] = 0;
-  queue.push([1, 0]);
-
-  while (queue.length) {
-    const [node, distance] = queue.shift();
-    const adjacantNodes = adjacantList[node];
-    for (let i = 0; i < adjacantNodes.length; i++) {
-      const adjacantNode = adjacantNodes[i];
-
-      if (!visited[adjacantNode]) {
-        visited[adjacantNode] = true;
-        queue.push([adjacantNode, distance + 1]);
-        distances[adjacantNode] = distance + 1;
-      }
+    const adjacantList = Array.from({length : n + 1}, () => [])
+    edge.forEach((v) => {
+        const [a, b] = v
+        adjacantList[a].push(b)
+        adjacantList[b].push(a)
+    })
+    
+    const distances = Array.from({length : n + 1}).fill(0)
+    const visited = Array.from({length : n + 1}).fill(false)
+    const queue = []
+    queue.push([1, 0])
+    visited[1] = true
+    
+    while(queue.length){
+        const [node, distance] = queue.shift()
+        distances[node] = distance
+        
+        for(const adjacantNode of adjacantList[node]){
+            if(!visited[adjacantNode]){
+                visited[adjacantNode] = true
+                queue.push([adjacantNode, distance + 1])
+            }
+        }
     }
-  }
-  const maxValue = distances.sort((a, b) => b - a)[0];
-  return distances.filter((v) => v === maxValue).length;
+    const maxDistance = Math.max(...distances)
+    
+    return distances.filter((v) => v === maxDistance).length
 }
