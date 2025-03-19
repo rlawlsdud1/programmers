8313160d150e0cea7a847e916ab4e180349ad0cd
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-03-19 17:55:47 +0900

    [level 1] Title: 폰켓몬, Time: 4.40 ms, Memory: 36.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,8 +1,9 @@
 function solution(nums) {
-  const canTake = nums.length / 2;
-  const ponketmonObj = {};
-  nums.forEach((v) => {
-    ponketmonObj[v] ? (ponketmonObj[v] += 1) : (ponketmonObj[v] = 1);
-  });
-  return Math.min(canTake, Object.keys(ponketmonObj).length);
+    const poketmonObj = {}
+    nums.forEach((v) =>{
+        poketmonObj[v] ? poketmonObj[v] += 1 : poketmonObj[v] = 1
+    })
+    const typeNum = Object.keys(poketmonObj).length
+    
+    return Math.min(nums.length / 2, typeNum)
 }
