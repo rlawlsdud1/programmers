9e9d26cf735b42b868ca97e9d96bb83ac6aeed52
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-03-19 19:25:37 +0900

    [level 3] Title: N으로 표현, Time: 17.53 ms, Memory: 39.3 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.js
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.js
@@ -1,34 +1,28 @@
-// N 사용 횟수
-//      1        N
-//      2        NN, N 과 N의 사칙연산
-//      3        NNN, N 과 NN의 사칙연산, NN과 N의 사칙연산
-//      4        NNNN, N과 NNN, NN과 NN, NNN과, N
-//      5        NNNNN, ...
-//      6        NNNNNM, ...
-//      7        NNNNNMN, ...
-//      8        NNNNNMNM, ...
-
 function solution(N, number) {
-  // dp[i] 는 N을 i번 사용했을 때의 값을 저장한 set
-  // Set의 요소를 삽입 순서대로 순회할 수 있다
-  const dp = Array.from({ length: 9 }, () => new Set());
-
-  for (let i = 1; i <= 8; i++) {
-    dp[i].add(Number(String(N).repeat(i)));
-
-    for (let j = 1; j < i; j++) {
-      for (const a of dp[i - j]) {
-        for (const b of dp[j]) {
-          dp[i].add(a * b);
-          dp[i].add(a + b);
-          dp[i].add(a - b);
-          dp[i].add(Math.floor(a / b));
+    const dp = Array.from({length : 9}, () => new Set())
+    
+    for(let i = 1; i <= 8; i++){
+        dp[i].add(Number(String(N).repeat(i)))
+        for(let j = 1; j < i; j++){
+            for(const a of dp[i - j]){
+                for(const b of dp[j]){
+                    dp[i].add(a * b)
+                    dp[i].add(a + b)
+                    dp[i].add(a - b)
+                    dp[i].add(Math.floor(a / b))
+                }
+            }
         }
-      }
+        if(dp[i].has(number)) return i
     }
-
-    if (dp[i].has(number)) return i;
-  }
-
-  return -1;
+    
+    return -1
 }
+
+
+// 예시 ( N = 5)
+// N을 1번 사용하는 경우 ) 5
+// N을 2번 사용하는 경우 ) 55, 5와 5의 사칙연산
+// N을 3번 사용하는 경우 ) 555, 5와 55의 사칙연산, 55와 5의 사칙연산
+// N을 4번 사용하는 경우 ) 5555, 5와 555의 사칙연산, 55와 55의 사칙연산, 555와 5의 사칙연산
+// ...
