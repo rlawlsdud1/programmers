232690ca0066c967cfff6bdf6f41fc034758b765
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-03-19 19:32:43 +0900

    [level 1] Title: 체육복, Time: 0.28 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,28 +1,33 @@
 function solution(n, lost, reserve) {
-  const lostArr = Array.from({ length: n }).fill(false);
-  const reserveArr = Array.from({ length: n }).fill(false);
-  lost.forEach((v) => {
-    lostArr[v - 1] = true;
-  });
-  reserve.forEach((v) => {
-    if (lostArr[v - 1]) {
-      lostArr[v - 1] = false;
-      reserveArr[v - 1] = false;
-    } else {
-      reserveArr[v - 1] = true;
+    const lostArr = Array.from({length : n}).fill(false)
+    const reserveArr = Array.from({length : n}).fill(false)
+    lost.forEach((v) => {
+        lostArr[v-1] = true
+    })
+    reserve.forEach((v) => {
+        reserveArr[v-1] = true
+    })
+    
+    // 잃어버렸는데 여벌이 있다면 잃어버린게 아니고, 여벌 또한 사라짐
+    for(let i=0; i<n; i++){
+        if(lostArr[i] && reserveArr[i]){
+            lostArr[i] = false
+            reserveArr[i] = false
+        }
     }
-  });
-  for (let i = 0; i < n; i++) {
-    if (lostArr[i]) {
-      if (reserveArr[i - 1]) {
-        lostArr[i] = false;
-        reserveArr[i - 1] = false;
-      } else if (reserveArr[i + 1]) {
-        lostArr[i] = false;
-        reserveArr[i + 1] = false;
-      }
+    
+    // 이제 여벌도 없는데 잃어버린 애들에게 체육복을 빌려줄 수 있는지 체크
+    for(let i=0; i<n; i++){
+        if(lostArr[i]){
+            if(reserveArr[i-1]){
+                lostArr[i] = false
+                reserveArr[i-1] = false
+            }else if(reserveArr[i+1]){
+                lostArr[i] = false
+                reserveArr[i+1] = false
+            }
+        }
     }
-  }
-
-  return lostArr.filter((v) => !v).length;
+    
+    return lostArr.filter((v) => v === false).length
 }
