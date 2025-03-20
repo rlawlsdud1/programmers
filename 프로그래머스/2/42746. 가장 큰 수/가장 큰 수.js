2fdd0277c9a27767097d579187ba54a7cd2ce526
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-03-20 15:29:46 +0900

    [level 2] Title: 가장 큰 수, Time: 582.31 ms, Memory: 46.6 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,6 +1,8 @@
 function solution(numbers) {
-  numbers.sort((a, b) =>
-    String(b).repeat(4).localeCompare(String(a).repeat(4))
-  );
-  return String(BigInt(numbers.join("")));
+    numbers.sort((a, b) => {
+        return String(b).repeat(4).slice(0,4) - String(a).repeat(4).slice(0,4) 
+    })
+    
+    return String(BigInt(numbers.join('')))
 }
+// numbers의 원소는 최대 네자리
