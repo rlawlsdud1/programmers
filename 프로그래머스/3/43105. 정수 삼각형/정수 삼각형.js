30d7ea959e9e9126c13f808a87abaad824152049
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-03-20 16:48:42 +0900

    [level 3] Title: 정수 삼각형, Time: 4.64 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -1,11 +1,9 @@
 function solution(triangle) {
-  triangle = triangle.reverse();
-
-  for (let i = 1; i < triangle.length; i++) {
-    for (let j = 0; j < triangle[i].length; j++) {
-      triangle[i][j] += Math.max(triangle[i - 1][j], triangle[i - 1][j + 1]);
+    triangle = triangle.reverse()
+    for(let i=1; i<triangle.length; i++){
+        for(let j=0; j<triangle[i].length; j++){
+            triangle[i][j] +=  Math.max(triangle[i-1][j], triangle[i-1][j+1])
+        }
     }
-  }
-
-  return triangle.at(-1)[0];
+    return triangle.at(-1)[0]
 }
