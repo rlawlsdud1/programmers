f9285d1f9203642c39222b077cf647a25933c1bb
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-03-20 15:16:15 +0900

    [level 2] Title: 기능개발, Time: 0.52 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,25 +1,21 @@
 function solution(progresses, speeds) {
-  // 1. progresses[i] 에 speeds[i] 를 계속 더한다
-  // 2. progresses[0] 이 100 넘는지 확인한다
-  // 3. 그렇다면 shift, 그 이후것도 100 넘는지 확인
-  // 4. 반복하고 안넘는다면 다시 1번 반복
-  const answer = [];
-
-  while (progresses.length) {
-    for (let i = 0; i < progresses.length; i++) {
-      progresses[i] += speeds[i];
+    const answer = [];
+    
+    while(progresses.length){
+        for(let i = 0; i < progresses.length; i++){
+            progresses[i] += speeds[i]
+        }    
+        let count = 0
+        while(progresses[0] >= 100){
+            progresses.shift()
+            speeds.shift()
+            count++
+        }
+        if(count) answer.push(count)
     }
-
-    let count = 0;
-    while (progresses[0] >= 100) {
-      progresses.shift();
-      speeds.shift();
-      count++;
-    }
-
-    if (count) {
-      answer.push(count);
-    }
-  }
-  return answer;
+    
+    return answer;
 }
+
+// 뒤에 있는 기능은 앞에 있는 기능이 배포될 때 함께 배포된다
+// 각 배포마다 몇 개의 기능이 배포되는지 return
