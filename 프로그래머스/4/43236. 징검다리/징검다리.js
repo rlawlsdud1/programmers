4a062928092cef9e91aa81e032a9b126919ddaac
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-03-21 20:17:28 +0900

    [level 4] Title: 징검다리, Time: 35.92 ms, Memory: 39.4 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.js
+++ 프로그래머스/4/43236. 징검다리/징검다리.js
@@ -1,12 +1,12 @@
 function solution(distance, rocks, n) {
-    let left = 0, right = distance
-    rocks.sort((a, b) => a - b)
-    rocks.unshift(0)
+    let left = 0
+    let right = distance
+    rocks.sort((a,b) => a-b)
+    rocks.unshift(0)    
     rocks.push(distance)
     
     while(left <= right){
-        const mid = Math.floor((left + right) / 2)
-        // 최소 mid만큼의 거리를 유지하기 위해서 몇 개의 돌을 제거해야하는지 체크할 것이다.
+        const mid = Math.floor((left + right) / 2)    
         
         let prev = 0
         let count = 0
@@ -19,13 +19,10 @@ function solution(distance, rocks, n) {
         }
         
         if(count > n){
-            // n개보다 더 많이 제거해야 한다면, 사이 거리를 줄여서 덜 제거하게끔 해야 한다
             right = mid - 1
         }else{
-            // n개보다 더 적게 제거 한다면 거리를 늘려서 더 제거하게끔 해야 한다
             left = mid + 1
         }
     }
-    
     return right
 }
