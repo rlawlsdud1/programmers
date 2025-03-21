a2c9f22249cb7404149e9710ae98fee9fdcbc31d
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-03-21 13:04:40 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 8.50 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
@@ -1,37 +1,40 @@
 function solution(n, wires) {
-  const adjacantList = Array.from({ length: n + 1 }, () => []);
-  wires.forEach((v) => {
-    const [from, at] = v;
-    adjacantList[from].push(at);
-    adjacantList[at].push(from);
-  });
-
-  let answer = Infinity;
-
-  for (let i = 0; i < wires.length; i++) {
-    const copyList = JSON.parse(JSON.stringify(adjacantList));
-    const [from, at] = wires[i];
-    copyList[from] = copyList[from].filter((v) => v !== at);
-    copyList[at] = copyList[at].filter((v) => v !== from);
-    const visited = Array.from({ length: n + 1 }).fill(false);
-
-    function DFS(node) {
-      let count = 1;
-
-      for (let i = 0; i < copyList[node].length; i++) {
-        const adjacantNode = copyList[node][i];
-        if (!visited[adjacantNode]) {
-          visited[adjacantNode] = true;
-          count += DFS(adjacantNode);
+    let answer = Infinity
+    const adjacantList = Array.from({length : n+1}, () => [])
+    wires.forEach((v) => {
+        const [a, b] = v
+        adjacantList[a].push(b)
+        adjacantList[b].push(a)
+    })
+    
+    function DFS(node, map, visited){
+        let count = 1
+        visited[node] = true
+        
+        for(const adjacantNode of map[node]){
+            if(!visited[adjacantNode]){
+                count += DFS(adjacantNode, map, visited)
+            }
         }
-      }
-
-      return count;
+        
+        return count
     }
-    visited[1] = true;
-    const count = DFS(1);
-    answer = Math.min(answer, Math.abs(n - 2 * count));
-  }
-
-  return answer;
+    
+    wires.forEach((v) => {
+        const [a, b] = v
+        const copiedMap = JSON.parse(JSON.stringify(adjacantList))
+        const visited = Array.from({length : n+1}).fill(false)
+        copiedMap[a] = copiedMap[a].filter((v) => v !== b)
+        copiedMap[b] = copiedMap[b].filter((v) => v !== a)
+        
+        const cntOfa = DFS(a, copiedMap, visited)
+        const cntOfb = DFS(b, copiedMap, visited)
+        answer = Math.min(answer, Math.abs(cntOfa - cntOfb))
+        
+        
+        // a에서 출발, b에서 출발
+        // 차의 절댓값과 answer 비교
+        // 더 작으면 answer 갱신
+    })
+    return answer
 }
