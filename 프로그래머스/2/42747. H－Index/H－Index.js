210c47769407b50c733e5f193c71a8ab2cfe4017
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-03-21 12:36:55 +0900

    [level 2] Title: H-Index, Time: 29.19 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,11 +1,15 @@
 function solution(citations) {
-  citations.sort((a, b) => a - b);
-  let answer = 0;
-  for (let i = 0; i < citations.length; i++) {
-    const count = citations.filter((e) => e >= citations[i]).length;
-    if (Math.min(count, citations[i]) > answer) {
-      answer = Math.min(count, citations[i]);
+    citations.sort((a,b) => a-b)
+    let answer = 0
+    
+    for(let i=0; i<citations.length; i++){
+        const count = citations.filter((v) => v >= citations[i]).length
+        if(Math.min(count, citations[i]) > answer){
+            answer = Math.min(count, citations[i])
+        }
     }
-  }
-  return answer;
+    
+    return answer;
 }
+
+// 각 원소는 논문의 인용 횟수
