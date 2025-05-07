b791683069212cb28f7bbbac4f74aac8ee5ab173
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-05-07 16:41:12 +0900

    [Gold V] Title: 평범한 배낭, Time: 944 ms, Memory: 92204 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.js
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.js
@@ -4,28 +4,31 @@ const filePath =
   process.platform === "linux"
     ? "/dev/stdin"
     : path.join(__dirname, "../input.txt");
-let input = fs
+const input = fs
   .readFileSync(filePath)
   .toString()
   .split("\n")
   .map((v) => v.trim());
 
 const [N, K] = input[0].split(" ").map(Number);
-const info = input.slice(1).map((v) => v.split(" ").map(Number));
-
+const info = input.slice(1, 1 + N).map((v) => v.split(" ").map(Number));
 const dp = Array.from({ length: N + 1 }, () =>
   Array.from({ length: K + 1 }).fill(0)
 );
 
 for (let i = 1; i <= N; i++) {
-  const currentObject = info[i - 1];
+  // 현재 관찰 중인 물건 정보
+  const [currentWeight, currentValue] = info[i - 1];
+
   for (let w = 1; w <= K; w++) {
-    if (w - currentObject[0] >= 0) {
+    // 지금 보고 있는 물건을 담을 수 있다면
+    if (w - currentWeight >= 0) {
       dp[i][w] = Math.max(
         dp[i - 1][w],
-        dp[i - 1][w - currentObject[0]] + currentObject[1]
+        dp[i - 1][w - currentWeight] + currentValue
       );
     } else {
+      // 담을 수 없다면
       dp[i][w] = dp[i - 1][w];
     }
   }
