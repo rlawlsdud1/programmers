1370ca2ed422eafbf3142aa79363c574a3398daa
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-05-12 22:13:55 +0900

    [Gold V] Title: 탑, Time: 448 ms, Memory: 108376 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.js
+++ 백준/Gold/2493. 탑/탑.js
@@ -13,19 +13,20 @@ const input = fs
 const N = Number(input[0]);
 const info = input[1].split(" ").map(Number);
 
-// 레이저 신호를 수신한 탑들의 번호
-// 왼쪽부터 시작해서, 일단 stack에 담자
-// 레이저를 쐈을 때, 가장 먼저 만날 수 있는 건물 번호를 알기 위해.
-
 const answer = Array.from({ length: N }).fill(0);
 const stack = [];
+
 for (let i = 0; i < N; i++) {
-  while (info[stack[stack.length - 1] - 1] < info[i]) {
+  const cur = info[i];
+
+  while (info[stack[stack.length - 1]] < cur) {
     stack.pop();
   }
-  stack.push(i + 1);
-  if (stack.length > 1) {
-    answer[i] = stack[stack.length - 2];
+
+  if (stack.length) {
+    answer[i] = stack[stack.length - 1] + 1;
   }
+  stack.push(i);
 }
+
 console.log(answer.join(" "));
