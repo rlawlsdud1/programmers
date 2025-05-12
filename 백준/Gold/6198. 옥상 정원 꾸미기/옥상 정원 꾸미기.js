48eea200b47320dcf49b3db55071e02ba292c0a9
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-05-12 22:39:48 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 192 ms, Memory: 24616 KB -BaekjoonHub

--- 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.js
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.js
@@ -4,26 +4,28 @@ const filePath =
   process.platform === "linux"
     ? "/dev/stdin"
     : path.join(__dirname, "../input.txt");
-let input = fs
+const input = fs
   .readFileSync(filePath)
   .toString()
   .split("\n")
   .map((v) => v.trim());
 
 const N = Number(input[0]);
-const heights = input.slice(1).map((v) => Number(v));
+const info = input.slice(1, 1 + N).map(Number);
 
-const stack = [];
 let answer = 0;
+const stack = [];
 
+// 값을 중복 사용 해야할 듯 하다.
+// 관점을 바꿔서 자기를 몇명이 볼 수 있는지 확인하자
 for (let i = 0; i < N; i++) {
-  const cur = heights[i];
+  const cur = info[i];
 
-  while (stack.length && stack[stack.length - 1] <= cur) {
+  while (stack[stack.length - 1] <= cur) {
     stack.pop();
   }
   answer += stack.length;
-  stack.push(cur);
+  stack.push(info[i]);
 }
 
 console.log(answer);
