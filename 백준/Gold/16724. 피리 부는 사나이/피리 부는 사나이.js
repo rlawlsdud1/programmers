6abea947f9c0126439434ddcefdd3f860d8d07fc
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-09-09 17:07:39 +0900

    [Gold III] Title: 피리 부는 사나이, Time: 3580 ms, Memory: 272488 KB -BaekjoonHub

---
+++ 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.js
@@ -0,0 +1,70 @@
+const fs = require("fs");
+const path = require("path");
+const filePath =
+  process.platform === "linux"
+    ? "/dev/stdin"
+    : path.join(__dirname, "../input.txt");
+let input = fs
+  .readFileSync(filePath)
+  .toString()
+  .split("\n")
+  .map((v) => v.trim());
+
+const [N, M] = input[0].split(" ").map(Number);
+const map = input.slice(1).map((v) => v.split(""));
+const directionObj = {
+  U: [-1, 0],
+  R: [0, 1],
+  D: [1, 0],
+  L: [0, -1],
+};
+
+const parent = {};
+
+for (let i = 0; i < N; i++) {
+  for (let j = 0; j < M; j++) {
+    parent[`${i},${j}`] = `${i},${j}`;
+  }
+}
+
+function find(target) {
+  if (target !== parent[target]) {
+    parent[target] = find(parent[target]);
+  }
+
+  return parent[target];
+}
+
+function union(x, y) {
+  const rootX = find(x);
+  const rootY = find(y);
+
+  if (rootX !== rootY) {
+    parent[rootX] = rootY;
+  }
+}
+
+for (let x = 0; x < N; x++) {
+  for (let y = 0; y < M; y++) {
+    const cur = parent[`${x},${y}`];
+    const dir = map[x][y];
+
+    const [nx, ny] = [x + directionObj[dir][0], y + directionObj[dir][1]];
+
+    if (nx >= 0 && ny >= 0 && nx < N && ny < M) {
+      const next = `${nx},${ny}`;
+
+      if (find(next) !== find(cur)) union(next, cur);
+    }
+  }
+}
+
+const answerSet = new Set();
+for (let i = 0; i < N; i++) {
+  for (let j = 0; j < M; j++) {
+    const cur = `${i},${j}`;
+    answerSet.add(find(parent[cur]));
+  }
+}
+
+console.log(answerSet.size);
