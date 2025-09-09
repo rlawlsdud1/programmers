e66b7cf5e3d40db155356b46be49309e111f57bb
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-09-09 19:08:22 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 8.65 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
@@ -1,60 +1,51 @@
 function solution(friends, gifts) {
-    var answer = 0;
-    const countOfFriends = friends.length
-    const nameObj = {}
-    friends.forEach((name, index) => {
-        nameObj[name] = index
-    })
-    
-    const giftTable = Array.from({length:countOfFriends}, ()=>
-                                 Array.from({length:countOfFriends}).fill(0)
-                                )
-    gifts.forEach((v) => {
-        const [from, to] = v.split(' ')
-        giftTable[nameObj[from]][nameObj[to]] += 1
-    })
-    
-    const giftObj = [] // 선물 지수
-    for(let i=0; i<countOfFriends; i++){
-        giftObj[i] = [0, 0]
-        
-        giftObj[i][0] += giftTable[i].reduce((acc, cur) => acc + cur, 0)
-    }
+  const nameToIdx = {};
+  friends.forEach((name, idx) => {
+    nameToIdx[name] = idx;
+  });
+  const cntOfFriends = friends.length;
+  const giftInfo = Array.from({ length: cntOfFriends }, () =>
+    Array.from({ length: cntOfFriends }).fill(0)
+  );
 
-    for(let j=0; j<countOfFriends; j++){
-        let count = 0
-        for(let i=0; i<countOfFriends; i++){
-            count += giftTable[i][j]
-        }
-        giftObj[j][1] = count
-    }
-    
-    for(let i=0; i<countOfFriends; i++){
-        giftObj[i] = giftObj[i][0] - giftObj[i][1]
-    }
-    
-    const nextMonthGift = Array.from({length:countOfFriends}).fill(0)
-    
-    for(let i=0; i<countOfFriends; i++){
-        for(let j=i+1; j<countOfFriends; j++){
-            const a = giftTable[i][j]
-            const b = giftTable[j][i]
-            
-            if((a>0 || b>0) && a !== b){
-                if(a>b){
-                    nextMonthGift[i] += 1
-                }else if(a<b){
-                    nextMonthGift[j] += 1
-                }
-            }else{
-                if(giftObj[i]>giftObj[j]){
-                    nextMonthGift[i] += 1
-                }else if(giftObj[i]<giftObj[j]){
-                    nextMonthGift[j] += 1
-            }       
-            }
-        }
-    }
+  gifts.forEach((v) => {
+    const [from, to] = v.split(" ");
+    giftInfo[nameToIdx[from]][nameToIdx[to]] += 1;
+  });
 
-    return Math.max(...nextMonthGift);
+  const giftPoints = Array.from({ length: cntOfFriends }, () => [0, 0]);
+
+  for (let i = 0; i < cntOfFriends; i++) {
+    giftPoints[i][0] += giftInfo[i].reduce((acc, cur) => acc + cur, 0);
+  }
+
+  for (let j = 0; j < cntOfFriends; j++) {
+    for (let i = 0; i < cntOfFriends; i++) {
+      giftPoints[j][1] += giftInfo[i][j];
+    }
+  }
+
+  for (let i = 0; i < cntOfFriends; i++) {
+    giftPoints[i] = giftPoints[i][0] - giftPoints[i][1];
+  }
+
+  const answerArr = Array.from({ length: cntOfFriends }).fill(0);
+  for (let i = 0; i < cntOfFriends; i++) {
+    for (let j = i + 1; j < cntOfFriends; j++) {
+      const a = giftInfo[i][j];
+      const b = giftInfo[j][i];
+      if ((a > 0 || b > 0) && a !== b) {
+        if (a > b) answerArr[i] += 1;
+        else if (a < b) answerArr[j] += 1;
+      } else {
+        const pointOfA = giftPoints[i];
+        const pointOfB = giftPoints[j];
+
+        if (pointOfA > pointOfB) answerArr[i] += 1;
+        else if (pointOfA < pointOfB) answerArr[j] += 1;
+      }
+    }
+  }
+
+  return Math.max(...answerArr);
 }
