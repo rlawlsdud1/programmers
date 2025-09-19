d8771f99d8213fb83f1673761d35fb945b862dfb
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2025-09-20 00:07:53 +0900

    [Silver I] Title: 신입 사원, Time: 3928 ms, Memory: 406032 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.js
+++ 백준/Silver/1946. 신입 사원/신입 사원.js
@@ -11,34 +11,32 @@ const input = fs
   .map((v) => v.trim());
 
 const T = Number(input[0]);
-const info = input.slice(1);
-let idx = 0;
-
+let idx = 1;
 for (let i = 0; i < T; i++) {
-  const N = Number(info[idx++]);
-  const scores = info.slice(idx, idx + N).map((v) => v.split(" ").map(Number));
+  const N = Number(input[idx++]);
+  const info = input.slice(idx, idx + N).map((v) => v.split(" ").map(Number));
 
-  // 문제에 주어지는 데이터는 점수가 아니라 순위
-  // 서류 등수 기준으로 오름 차순
-  scores.sort((a, b) => a[0] - b[0]);
+  // main logic
+  info.sort((a, b) => {
+    if (a[0] === b[0]) return a[1] - b[1];
+    return a[0] - b[0];
+  });
 
-  let interviewScore = scores[0][1];
-  let count = 1;
+  // 서류 성적부터 석차 순으로 오름차순 정렬
+  // 어떤 지원자는 앞에 있는 지원자보다 석차는 밀린다. 즉, 면접 순위라도 높아야 한다.
+  // info 배열을 순회하면서 면접 석차 제일 높은걸로 갱신한다.
+  // 그 값보다 낮자면 석차도, 면접도 밀리는 것이고 선발될 수 없다.
 
-  for (let j = 1; j < N; j++) {
-    const curScore = scores[j];
+  let answer = 0;
+  let max_interview_rank = Infinity;
+  info.forEach((v) => {
+    const [_, b] = v;
 
-    // 면접 점수라도 더 좋아야 선발된다.
-    // 이 분기를 탈 때 인터뷰 순위를 갱신해주는 이유는,
-    // 처음에 서류 등수 기준으로 오름차순 정렬했으니
-    // 면접이라도 더 잘봐야 선발될텐데
-    // 마지막으로 선발된 애의 면접 등수가 기준점이 될 수 밖에 없다
-    // 다음으로 선발될 애는 마지막으로 선발된 애보다 면접을 더 잘봐야 한다
-    if (curScore[1] < interviewScore) {
-      count++;
-      interviewScore = curScore[1];
-    }
-  }
-  console.log(count);
+    if (max_interview_rank > b) answer++;
+    max_interview_rank = Math.min(max_interview_rank, b);
+  });
+
+  console.log(answer);
+
   idx += N;
 }
