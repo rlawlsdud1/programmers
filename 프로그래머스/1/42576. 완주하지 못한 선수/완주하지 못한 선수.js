7c13e302de04566d5c77be2cb32a365adc9a8224
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2026-03-07 18:59:21 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 52.67 ms, Memory: 71.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,21 +1,13 @@
 function solution(participant, completion) {
-    const participantObj = {}
-    const completionObj = {}
-    
-    participant.forEach((v) => {
-        participantObj[v] ? participantObj[v] += 1 : participantObj[v] = 1
-    })
-    completion.forEach((v) => {
-        completionObj[v] ? completionObj[v] += 1 : completionObj[v] = 1
-    })
-    const keyOfParticipant = Object.keys(participantObj)
-    for(let i=0; i<keyOfParticipant.length; i++){
-        if(participantObj[keyOfParticipant[i]] > completionObj[keyOfParticipant[i]]){
-            return keyOfParticipant[i]
-        }else if(!completionObj[keyOfParticipant[i]]){
-            return keyOfParticipant[i]
-        }
-              
-    }
+  const hashTable = {};
+
+  completion.forEach((v) => {
+    hashTable[v] ? (hashTable[v] += 1) : (hashTable[v] = 1)
+  });
+
+  for (const player of participant) {
+    if (!hashTable[player]) return player;
+
+    hashTable[player] -= 1
+  }
 }
-// 단 한 명의 선수를 제외하고는 모든 선수가 마라톤을 완주
