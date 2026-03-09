2d703cbb13c9d5b556c43d8888abd5f57292f512
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2026-03-09 19:44:28 +0900

    [level 2] Title: 프로세스, Time: 0.58 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,13 +1,30 @@
 function solution(priorities, location) {
-  // 중요도가 높을수록 먼저 실행된다
-  const locAndPrior = priorities.map((v, i) => [v, i]);
-  const complete = [];
-  while (locAndPrior.length) {
-    if (locAndPrior.slice(1).find((v) => v[0] > locAndPrior[0][0])) {
-      locAndPrior.push(locAndPrior.shift());
-    } else {
-      complete.push(locAndPrior.shift());
+  // 위치를 기억하기 위한 parsing
+  priorities.forEach((v, i) => {
+    priorities[i] = [v, i];
+  });
+
+  let count = 0; // 몇번째로 실행되는지
+
+  while (priorities.length) {
+    const n = priorities.length;
+    const [target_priority, target_location] = priorities.shift();
+
+    let can_execute = true;
+    for (let i = 0; i < n - 1; i++) {
+      const priority = priorities[i][0];
+
+      // 우선순위 높은게 있다면 다시 큐에 넣음
+      if (target_priority < priority) {
+        priorities.push([target_priority, target_location]);
+        can_execute = false;
+        break;
+      }
+    }
+
+    if (can_execute) {
+      count++;
+      if (target_location === location) return count;
     }
   }
-  return complete.findIndex((v) => v[1] === location) + 1;
 }
