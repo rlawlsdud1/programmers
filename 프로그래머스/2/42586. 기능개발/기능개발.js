23206db79708dead5297409d875400842effe831
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2026-03-09 10:35:01 +0900

    [level 2] Title: 기능개발, Time: 0.53 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,21 +1,22 @@
 function solution(progresses, speeds) {
-    const answer = [];
-    
-    while(progresses.length){
-        for(let i = 0; i < progresses.length; i++){
-            progresses[i] += speeds[i]
-        }    
-        let count = 0
-        while(progresses[0] >= 100){
-            progresses.shift()
-            speeds.shift()
-            count++
-        }
-        if(count) answer.push(count)
-    }
-    
-    return answer;
-}
+  const answer = [];
 
-// 뒤에 있는 기능은 앞에 있는 기능이 배포될 때 함께 배포된다
-// 각 배포마다 몇 개의 기능이 배포되는지 return
+  while (progresses.length) {
+    const n = progresses.length;
+    for (let i = 0; i < n; i++) {
+      progresses[i] += speeds[i];
+    }
+
+    let count = 0;
+
+    while (progresses[0] >= 100) {
+      progresses.shift();
+      speeds.shift();
+      count++;
+    }
+
+    if (count) answer.push(count);
+  }
+
+  return answer;
+}
