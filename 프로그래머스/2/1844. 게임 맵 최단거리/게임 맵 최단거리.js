3457d1a5c804d22da13a345f103dff6afcf0ad0f
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2026-05-14 19:17:23 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 21.76 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,6 +1,8 @@
 function solution(maps) {
-  // n x m 형태의 map
-  // 필수적으로
+  let answer = 0;
+
+  const n = maps.length;
+  const m = maps[0].length;
   const directions = [
     [1, 0],
     [-1, 0],
@@ -8,36 +10,35 @@ function solution(maps) {
     [0, -1],
   ];
 
+  const visited = Array.from({ length: n }, () =>
+    Array.from({ length: m }).fill(false),
+  );
+
   const queue = [];
   queue.push([0, 0, 1]);
-  maps[0][0] = 0;
+  visited[0][0] = true;
 
-  // 하나씩 꺼낼거임
   while (queue.length) {
-    const [x, y, distance] = queue.shift();
+    const [x, y, count] = queue.shift();
 
-    if (x === maps.length - 1 && y === maps[0].length - 1) {
-      return distance;
-    }
+    if (x === n - 1 && y === m - 1) return count;
 
-    for (let i = 0; i < directions.length; i++) {
-      const [nx, ny] = [x + directions[i][0], y + directions[i][1]];
+    for (const direction of directions) {
+      const [nx, ny] = [x + direction[0], y + direction[1]];
 
-      // 실제로 갈 수 있는지를 체크할거임
       if (
         nx >= 0 &&
         ny >= 0 &&
-        nx < maps.length &&
-        ny < maps[0].length &&
-        maps[nx][ny] === 1
+        nx < n &&
+        ny < m &&
+        maps[nx][ny] &&
+        !visited[nx][ny]
       ) {
-        // 실제로 갈 수 있다면 다시 queue에 넣어줄거임
-        queue.push([nx, ny, distance + 1]);
-        // maps 를 바꿔줄거임.
-        maps[nx][ny] = 0;
+        queue.push([nx, ny, count + 1]);
+        visited[nx][ny] = true;
       }
     }
-  }//
+  }
 
   return -1;
 }
