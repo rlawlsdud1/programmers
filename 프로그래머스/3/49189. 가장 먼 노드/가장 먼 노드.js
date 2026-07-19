f15d3cdc72651adcb3f716f1d525851cc58a4606
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2026-07-19 15:02:32 +0900

    [level 3] Title: 가장 먼 노드, Time: 260.16 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
@@ -1,29 +1,39 @@
-function solution(n, edge) {
-    const adjacantList = Array.from({length : n + 1}, () => [])
-    edge.forEach((v) => {
+function solution(n, vertex) {
+    let answer = 0;
+    
+    const graph = {}
+    vertex.forEach((v) => {
         const [a, b] = v
-        adjacantList[a].push(b)
-        adjacantList[b].push(a)
+        graph[a] ? graph[a].push(b) : graph[a] = [b]
+        graph[b] ? graph[b].push(a) : graph[b] = [a]
     })
     
-    const distances = Array.from({length : n + 1}).fill(0)
     const visited = Array.from({length : n + 1}).fill(false)
     const queue = []
     queue.push([1, 0])
     visited[1] = true
     
-    while(queue.length){
-        const [node, distance] = queue.shift()
-        distances[node] = distance
-        
-        for(const adjacantNode of adjacantList[node]){
-            if(!visited[adjacantNode]){
-                visited[adjacantNode] = true
-                queue.push([adjacantNode, distance + 1])
-            }
-        }
-    }
-    const maxDistance = Math.max(...distances)
+    const lengthArr = Array.from({length : n + 1}).fill(0)
     
-    return distances.filter((v) => v === maxDistance).length
+    while(queue.length){
+        const [node, count] = queue.shift()
+        
+        graph[node].forEach((v) => {
+            if(!visited[v]){
+                visited[v] = true
+                lengthArr[v] = count
+                queue.push([v, count + 1])
+            }
+        })
+    }
+
+    lengthArr.sort((a, b) => b - a)
+    const standard = lengthArr[0]
+    
+    for(let i = 0; i < n; i++){
+        if(lengthArr[i] !== standard) break
+        answer++
+    }
+    
+    return answer;
 }
