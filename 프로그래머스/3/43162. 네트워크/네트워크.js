28d0372201e9c7003a9c0cc171f4c8ee0fd0230e
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2026-07-19 14:37:33 +0900

    [level 3] Title: 네트워크, Time: 0.44 ms, Memory: 43.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,24 +1,34 @@
 function solution(n, computers) {
-    let count = 0
+    let answer = 0
     
-    function DFS(node){
-        for(let i=0; i<n; i++){
-            // 방문 안했고 node와 연결돼있다면
-            if(!visited[i] && computers[node][i]){
-                visited[i] = true
-                DFS(i)
+    const graph = {}
+    
+    for(let i = 0; i < n; i++){
+        for(let j = i+1; j < n; j++){
+            if(computers[i][j] !== 0){
+                graph[i+1] ? graph[i+1].push(j+1) : graph[i+1] = [j+1]
+                graph[j+1] ? graph[j+1].push(i+1) : graph[j+1] = [i+1]
             }
-        }    
-    }
-    
-    const visited = Array.from({length : n}).fill(false)
-    for(let i=0; i<n; i++){
-        if(!visited[i]){
-            visited[i] = true
-            DFS(i)
-            count++
         }
     }
     
-    return count
+    const visited = Array.from({length : n+1}).fill(false)
+    
+    function DFS(node, visited){
+        graph[node]?.forEach((adjacantNode) => {
+            if(!visited[adjacantNode]){
+                visited[adjacantNode] = true
+                DFS(adjacantNode, visited)
+            }
+        })
+    }
+    
+    for(let i = 1; i <= n; i++){
+        if(!visited[i]){
+            answer++
+            DFS(i, visited)
+        }
+    }
+    
+    return answer
 }
