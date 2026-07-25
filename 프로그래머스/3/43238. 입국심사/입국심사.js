06d495f4f84bd74f11d5ca6ba36e966cbffae65e
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2026-07-25 19:57:24 +0900

    [level 3] Title: 입국심사, Time: 77.67 ms, Memory: 55.1 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.js
+++ 프로그래머스/3/43238. 입국심사/입국심사.js
@@ -1,20 +1,25 @@
 function solution(n, times) {
-    let right = Math.max(...times) * n
+    let answer = Infinity
+    
     let left = 0
+    let right = Math.max(...times) * n
     
     while(left <= right){
-        const mid = Math.floor((left+right)/2)
+        const mid = Math.floor((left + right) / 2)
+        
         let sum = 0
-        times.forEach((v) => {
-            sum += Math.floor(mid / v)
+        times.forEach((t) => {
+            // 주어진 시간동안 해당 심사대에서 몇 명 처리할 수 있는지
+            sum += Math.floor(mid / t)
         })
         
         if(sum >= n){
+            answer = Math.min(answer, mid)
             right = mid - 1
         }else{
             left = mid + 1
         }
     }
     
-    return left
+    return answer;
 }
