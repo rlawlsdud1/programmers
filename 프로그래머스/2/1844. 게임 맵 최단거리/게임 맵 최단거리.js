14d589be86dd50bbeb156e84afdae9763f8f3dfc
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2026-09-21 21:50:43 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 25.99 ms, Memory: 56.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,44 +1,51 @@
 function solution(maps) {
-  let answer = 0;
-
-  const n = maps.length;
-  const m = maps[0].length;
-  const directions = [
-    [1, 0],
-    [-1, 0],
-    [0, 1],
-    [0, -1],
-  ];
-
-  const visited = Array.from({ length: n }, () =>
-    Array.from({ length: m }).fill(false),
-  );
-
-  const queue = [];
-  queue.push([0, 0, 1]);
-  visited[0][0] = true;
-
-  while (queue.length) {
-    const [x, y, count] = queue.shift();
-
-    if (x === n - 1 && y === m - 1) return count;
-
-    for (const direction of directions) {
-      const [nx, ny] = [x + direction[0], y + direction[1]];
-
-      if (
-        nx >= 0 &&
-        ny >= 0 &&
-        nx < n &&
-        ny < m &&
-        maps[nx][ny] &&
-        !visited[nx][ny]
-      ) {
-        queue.push([nx, ny, count + 1]);
-        visited[nx][ny] = true;
-      }
+    class Queue {
+        constructor(){
+            this.queue = []
+            this.head = 0
+        }
+        
+        size() {
+            return this.queue.length - this.head
+        }
+        
+        push(value) {
+            this.queue.push(value)
+        }
+        
+        pop() {
+            if(this.size() === 0) return undefined
+            return this.queue[this.head++]
+        }
+        
+        peek() {
+            return this.queue[this.head]
+        }
     }
-  }
-
-  return -1;
+    let answer = 0;
+    const directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+    const [n, m] = [maps.length, maps[0].length] 
+    
+    const visited = Array.from({length : n}, () => Array.from({length : m}).fill(false))
+    const queue = new Queue()
+    
+    queue.push([0, 0, 1])
+    while(queue.size() > 0) {
+        const [x, y, count] = queue.pop()
+        
+        if(x === n - 1 && y === m - 1) return count
+        
+        for(const direction of directions){
+            const [nx, ny] = [x + direction[0], y + direction[1]]
+            if(nx >= 0 && ny >= 0 && nx < n && ny < m) {
+                if(!visited[nx][ny] && maps[nx][ny]){
+                    queue.push([nx, ny, count + 1])
+                    visited[nx][ny] = true
+                }
+            }
+        }
+        
+    }
+    
+    return -1;
 }
