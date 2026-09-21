7aa862b94f512ef3579a3cfb0206074c3a7b43a5
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2026-09-21 18:10:41 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 20.68 ms, Memory: 50.6 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -1,36 +1,61 @@
 function solution(bridge_length, weight, truck_weights) {
-  const n = truck_weights.length;
-  let answer = 0;
-
-  const bridge = Array.from({ length: bridge_length }).fill(0);
-  let sum = 0; // 현재 다리 위에 있는 트럭들의 무게 합
-  let count = 0; // 다리를 지난 트럭 수
-
-  while (count < n) {
-    const next = truck_weights[0];
-
-    // 이번에 다리를 건너는 트럭이 있는지
-    if (bridge[0]) {
-      if (sum - bridge[0] + next <= weight) {
-        bridge.push(next);
-        sum += next;
-        truck_weights.shift();
-      } else bridge.push(0);
-
-      sum -= bridge[0];
-      count++;
-    } else {
-      if (sum + next <= weight) {
-        bridge.push(next);
-        sum += next;
-        truck_weights.shift();
-      } else bridge.push(0);
+    let answer = 0;
+    
+    class Queue {
+        constructor() {
+            this.queue = []
+            this.head = 0
+        }
+        
+        size() {
+            return this.queue.length - this.head
+        }
+        
+        push(value) {
+            this.queue.push(value)
+        }
+        
+        pop() {
+            if(this.size() === 0) return undefined
+            return this.queue[this.head++]
+        }
+        
+        peek() {
+            return this.queue[this.head]
+        }
     }
-
-    bridge.shift();
-
-    answer++;
-  }
-
-  return answer;
+    
+    const queue = new Queue()
+    for(let i = 0; i < bridge_length; i++){
+        queue.push(0)
+    }
+    
+    let total_weight = 0 // 다리 위에 있는 트럭 무게 합
+    let total_count = 0 // 다리 위에 있는 트럭 수
+    let pointer = 0 // 대기 트럭 인덱스
+    let exit_count = 0
+    
+    while(exit_count < truck_weights.length) {
+        const peek = queue.pop()
+            
+        if(peek){
+            total_weight -= peek
+            total_count--    
+            exit_count++
+        }
+        if(total_weight + truck_weights[pointer] <= weight) {
+           if(total_count + 1 <= bridge_length) {
+                total_weight += truck_weights[pointer]
+                queue.push(truck_weights[pointer++])
+                total_count++   
+           }
+        } else { 
+            queue.push(0)
+        }
+        
+        answer++
+    }
+    
+    
+    return answer;
 }
