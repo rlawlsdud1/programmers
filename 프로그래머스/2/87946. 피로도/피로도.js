22a7c8c0272645204496711abeb0253234a8f0f1
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2026-09-21 19:52:49 +0900

    [level 2] Title: 피로도, Time: 49.65 ms, Memory: 50.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,21 +1,23 @@
 function solution(k, dungeons) {
-  let answer = 0;
-  const n = dungeons.length;
-
-  function DFS(cur_hp, visited, count) {
-    answer = Math.max(answer, count);
-
-    for (let i = 0; i < n; i++) {
-      const [required_hp, consumed_hp] = dungeons[i];
-      if (!visited.has(i) && cur_hp >= required_hp) {
-        visited.add(i);
-        DFS(cur_hp - consumed_hp, visited, count + 1);
-        visited.delete(i);
-      }
+    let answer = 0
+    
+    const n = dungeons.length
+    
+    function DFS(visited, hp, count) {
+        answer = Math.max(answer, count)
+        
+        for(let i = 0; i < n; i++){
+            const [required_hp, cost] = dungeons[i]
+            
+            if(required_hp <= hp && !visited.has(i)) {
+                visited.add(i)
+                DFS(visited, hp - cost, count + 1)
+                visited.delete(i)
+            }
+        }
     }
-  }
-
-  DFS(k, new Set(), 0);
-
-  return answer;
+    
+    DFS(new Set(), k, 0)
+    
+    return answer;
 }
