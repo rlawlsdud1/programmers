68520649e74e20dff1b681eab27b944336d77fea
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2026-09-21 21:13:42 +0900

    [level 2] Title: 무인도 여행, Time: 27.50 ms, Memory: 51.9 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.js
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.js
@@ -1,48 +1,34 @@
 function solution(maps) {
-  maps = maps.map((v) => {
-    return v.split("").map((e) => (isNaN(Number(e)) ? e : Number(e)));
-  });
-
-  const answer = [];
-  const directions = [
-    [1, 0],
-    [-1, 0],
-    [0, 1],
-    [0, -1],
-  ];
-
-  const [n, m] = [maps.length, maps[0].length];
-  const visited = Array.from({ length: n }, () =>
-    Array.from({ length: m }).fill(false),
-  );
-
-  function DFS(x, y) {
-    let sum = maps[x][y];
-
-    for (const direction of directions) {
-      const [nx, ny] = [x + direction[0], y + direction[1]];
-
-      if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
-        if (!visited[nx][ny] && maps[nx][ny] !== "X") {
-          visited[nx][ny] = true;
-          sum += DFS(nx, ny);
+    const answer = [];
+    const [n, m] = [maps.length, maps[0].length]
+    const visited = Array.from({length : n}, () => Array.from({length : m}).fill(false))
+    
+    const directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+    function DFS(x, y) {
+        let cumulative = Number(maps[x][y])
+        
+        for(const direction of directions){
+            const [nx, ny] = [x + direction[0], y + direction[1]]
+            if(nx >= 0 && ny >= 0 && nx < n && ny < m){
+                if(!visited[nx][ny] && maps[nx][ny] !== 'X'){
+                    visited[nx][ny] = true
+                    cumulative += DFS(nx, ny)
+                }
+            }
         }
-      }
+        
+        return cumulative
     }
-
-    return sum;
-  }
-
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < m; j++) {
-      if (maps[i][j] !== "X" && !visited[i][j]) {
-        visited[i][j] = true;
-        const sum = DFS(i, j);
-        answer.push(sum);
-      }
+    
+    for(let i = 0; i < n; i++){
+        for(let j = 0; j < m; j++){
+            if(!visited[i][j] && maps[i][j] !== 'X'){
+                visited[i][j] = true
+                const result = DFS(i, j, maps[i][j])
+                answer.push(result)
+            }
+        }
     }
-  }
-
-  if (!answer.length) return [-1];
-  return answer.sort((a, b) => a - b);
+    
+    return answer.length ? answer.sort((a, b) => a - b) : [-1]
 }
