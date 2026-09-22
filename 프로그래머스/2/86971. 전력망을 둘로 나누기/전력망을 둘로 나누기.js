2f565006b6ae88aa6e17123fa28b11fe40d4eec1
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2026-09-22 20:35:45 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 15.06 ms, Memory: 50.4 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
@@ -1,52 +1,39 @@
 function solution(n, wires) {
     let answer = Infinity
     
+    // 2차원 배열로 하는 게 훨씬 효율적일 듯
     const graph = {}
+    
     wires.forEach((v) => {
         const [a, b] = v
         graph[a] ? graph[a].push(b) : graph[a] = [b]
         graph[b] ? graph[b].push(a) : graph[b] = [a]
     })
     
-    wires.forEach((v) => {
-        const [a, b] = v
-        // 간선 하나씩 끊어가면서 DFS 돌리고 각 트리의 노드 수 구하기
+    function DFS(node, graph, visited) {
+        let count = 1
         
-        graph[a] = graph[a].filter((v) => v !== b)
-        graph[b] = graph[b].filter((v) => v !== a)
-        
-        let [first, second] = [0, 0]
-        const visited = Array.from({length : n + 1}).fill(false)
-        
-        for(let i = 1; i <= n; i++){
-            if(!visited[i]){
-                visited[i] = true
-                const total_count = DFS(i, graph, visited)
-                
-                if(!first) first = total_count
-                else second = total_count
+        for(const adjacantNode of graph[node]){
+            if(!visited.has(adjacantNode)){
+                visited.add(adjacantNode)
+                count += DFS(adjacantNode, graph, visited)
             }
         }
-
-        answer = Math.min(answer, Math.abs(first - second))
         
-        // 그래프 원복
-        graph[a].push(b)
-        graph[b].push(a)
+        return count
+    }
+    
+    wires.forEach((v) => {
+        const [a, b] = v
+        const copied_graph = JSON.parse(JSON.stringify(graph))
+        copied_graph[a] = copied_graph[a].filter((v) => v !== b)
+        copied_graph[b] = copied_graph[b].filter((v) => v !== a)
+        
+        const visited = new Set()
+        visited.add(1)
+        const count = DFS(1, copied_graph, visited)
+        answer = Math.min(Math.abs(n - 2 * count), answer)
     })
     
     return answer;
-}
-
-function DFS(node, graph, visited){
-    let count = 1
-    
-    graph[node].forEach((v) => {
-        if(!visited[v]){
-            visited[v] = true
-            count += DFS(v, graph, visited)
-        }
-    })
-    
-    return count
 }
