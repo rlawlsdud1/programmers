b688d50fe43c19bd89b7791a3acad58c1767b0a8
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2026-09-22 20:38:12 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 3.27 ms, Memory: 50.7 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
@@ -1,7 +1,6 @@
 function solution(n, wires) {
     let answer = Infinity
     
-    // 2차원 배열로 하는 게 훨씬 효율적일 듯
     const graph = {}
     
     wires.forEach((v) => {
@@ -26,12 +25,14 @@ function solution(n, wires) {
     wires.forEach((v) => {
         const [a, b] = v
         const copied_graph = JSON.parse(JSON.stringify(graph))
+        
         copied_graph[a] = copied_graph[a].filter((v) => v !== b)
         copied_graph[b] = copied_graph[b].filter((v) => v !== a)
         
         const visited = new Set()
         visited.add(1)
         const count = DFS(1, copied_graph, visited)
+        
         answer = Math.min(Math.abs(n - 2 * count), answer)
     })
     
