586983c8318b65218231d432fa4215b4d0bfce51
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2026-09-22 20:11:19 +0900

    [level 2] Title: 모음 사전, Time: 0.80 ms, Memory: 48.4 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -1,25 +1,18 @@
 function solution(word) {
-  const everyWordsSet = new Set();
-  const vowel = ["A", "E", "I", "O", "U"];
-
-  function BT(str) {
-    if (str.length === 5) {
-      everyWordsSet.add(str);
-      return;
+    const vowels = ['A', 'E', 'I', 'O', 'U']
+    const dictionary = []
+    
+    function BT(word) {
+        for(let i = 0; i < 5; i++) {
+            if(word.length <= 4){
+                const next = word + vowels[i]
+                dictionary.push(next)  
+                BT(next)    
+            }
+        }
     }
-
-    for (let i = 0; i < 5; i++) {
-      str += vowel[i];
-      everyWordsSet.add(str);
-      BT(str);
-      str = str.slice(0, -1);
-    }
-  }
-
-  BT("");
-
-  const everyWordsArr = [...everyWordsSet];
-  everyWordsArr.sort();
-
-  return everyWordsArr.findIndex((v) => v === word) + 1;
+    
+    BT('')
+    
+    return dictionary.findIndex((v) => v === word) + 1
 }
