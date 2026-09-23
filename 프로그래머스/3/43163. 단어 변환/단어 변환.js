55c936f4c4cff4855b00b5b0f46a0519ec1e56e3
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2026-09-23 19:55:16 +0900

    [level 3] Title: 단어 변환, Time: 1.35 ms, Memory: 43.6 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -1,29 +1,61 @@
 function solution(begin, target, words) {
-  const visited = Array.from({ length: words.length }).fill(false);
-  let answer = Infinity;
-  function DFS(word, count) {
-    if (word === target) {
-      answer = Math.min(answer, count);
+    if(!words.find((v) => v === target)) return 0
+    
+    function can_change(word1, word2) {
+        let count = 0
+        const word1_to_arr = word1.split('')
+        const word2_to_arr = word2.split('')
+        
+        for(let i = 0; i < word1.length; i++){
+            if(word1_to_arr[i] !== word2_to_arr[i]) count++
+            
+            if(count > 1) return false
+        }
+        
+        return count === 0 ? false : true
     }
-    for (let i = 0; i < words.length; i++) {
-      if (!visited[i] && checkCanChange(word, words[i])) {
-        visited[i] = true;
-        DFS(words[i], count + 1);
-
-        visited[i] = false;
-      }
+    
+    const graph = {}
+    
+    for(let i = 0; i < words.length - 1; i++){
+        for(let j = i + 1; j < words.length; j++){
+            const word1 = words[i]
+            const word2 = words[j]
+            
+            if(can_change(word1, word2)) {
+                graph[word1] ? graph[word1].push(word2) : graph[word1] = [word2]
+                graph[word2] ? graph[word2].push(word1) : graph[word2] = [word1]
+            }
+        }
     }
-  }
-  DFS(begin, 0);
-  if (answer !== Infinity) return answer;
-  return 0;
-}
+    
+    words.forEach((v) => {
+        const word = v
+        
+        if(can_change(word, begin)) {
+            graph[word] ? graph[word].push(begin) : graph[word] = [begin]
+            graph[begin] ? graph[begin].push(word) : graph[begin] = [word]
+        }
+    })
+    
+    const queue = []
+    queue.push([begin, 0])
+    
+    const visited = new Set()
+    visited.add(begin)
+    
+    while(queue.length) {
+        const [word, count] = queue.shift()
+        
+        if(word == target) return count
+        
+        for(const nextWord of graph[word] || []) {
+            if(!visited.has(nextWord)) {
+                visited.add(nextWord)
+                queue.push([nextWord, count + 1])
+            }
+        }
+    }
 
-function checkCanChange(str1, str2) {
-  let count = 0;
-  for (let i = 0; i < str1.length; i++) {
-    if (str1[i] !== str2[i]) count++;
-  }
-  if (count === 1) return true;
-  return false;
+    return 0
 }
