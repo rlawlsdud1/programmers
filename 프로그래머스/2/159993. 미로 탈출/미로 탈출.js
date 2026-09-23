3bf460c70bde7ba9b519780f78c95a2a099004f9
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2026-09-23 14:31:40 +0900

    [level 2] Title: 미로 탈출, Time: 25.68 ms, Memory: 53.9 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.js
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.js
@@ -1,61 +1,70 @@
 function solution(maps) {
-  const directions = [
-    [1, 0],
-    [-1, 0],
-    [0, 1],
-    [0, -1],
-  ];
-
-  let startX, startY;
-
-  const N = maps.length;
-  const M = maps[0].length;
-
-  for (let i = 0; i < N; i++) {
-    for (let j = 0; j < M; j++) {
-      if (maps[i][j] === "S") {
-        startX = i;
-        startY = j;
-      }
+    class Queue {
+        constructor() {
+            this.queue = []
+            this.head = 0
+        }
+        
+        size() {
+            return this.queue.length - this.head
+        }
+        
+        push(value) {
+            this.queue.push(value)
+        }
+        
+        pop() {
+            if(this.size() === 0) return undefined
+            return this.queue[this.head++]
+        }
+        
+        peek() {
+            return this.queue[this.head]
+        }
     }
-  }
-
-  const queue = [];
-  queue.push([startX, startY, false, 0]);
-  const visited = Array.from({ length: N }, () =>
-    Array.from({ length: M }, () => [false, false])
-  );
-  visited[startX][startY][0] = true;
-
-  let count = 0;
-
-  while (queue.length) {
-    const [x, y, lever, count] = queue.shift();
-
-    if (maps[x][y] === "E" && lever) {
-      return count;
+    
+    const [n, m] = [maps.length, maps[0].length]
+    
+    const directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+    
+    const queue = new Queue()
+    
+    const visited = Array.from({length : n}, () => Array.from({length : m}, () => [false, false]))
+    
+    for(let i = 0; i < n; i++){
+        for(let j = 0; j < m; j++){
+            if(maps[i][j] === 'S') {
+                queue.push([i, j, 0, false])
+                visited[i][j][0] = true
+            }
+        }
     }
-
-    for (const direction of directions) {
-      const [nx, ny] = [x + direction[0], y + direction[1]];
-
-      if (
-        nx >= 0 &&
-        ny >= 0 &&
-        nx < N &&
-        ny < M &&
-        maps[nx][ny] !== "X" &&
-        !visited[nx][ny][lever]
-      ) {
-        visited[nx][ny][lever] = true;
-
-        if (maps[nx][ny] === "L") queue.push([nx, ny, !lever, count + 1]);
-        else queue.push([nx, ny, lever, count + 1]);
-      }
+    
+    
+    while(queue.size() > 0) {
+        const [x, y, count, isOpen] = queue.pop()
+        
+        if(maps[x][y] === 'E' && isOpen) return count
+        
+        for(const direction of directions) {
+            const [nx, ny] = [x + direction[0], y + direction[1]]
+            
+            if(nx >= 0 && ny >= 0 && nx < n && ny < m && maps[nx][ny] !== 'X') {
+                if(!visited[nx][ny][0] && !isOpen) {
+                    visited[nx][ny][0] = true
+                    
+                    if(maps[nx][ny] === 'L') {
+                        queue.push([nx, ny, count + 1, true])
+                    }else {
+                        queue.push([nx, ny, count + 1, isOpen])
+                    }
+                }else if(!visited[nx][ny][1] && isOpen) {
+                    visited[nx][ny][1] = true
+                    queue.push([nx, ny, count + 1, isOpen])
+                }
+            }
+        }
     }
-  }
-
-  return -1;
+    
+    return -1
 }
-
-console.log(solution(["LOOXS", "OOOOX", "OOOOO", "OOOOO", "EOOOO"]));
