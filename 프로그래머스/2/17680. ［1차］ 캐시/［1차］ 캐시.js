deae37989b8d15455abed3a507b84ad50b82b253
김진영 <159886577+rlawlsdud1@users.noreply.github.com>
2026-09-25 18:15:30 +0900

    [level 2] Title: [1차] 캐시, Time: 60.85 ms, Memory: 54 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -1,34 +1,22 @@
 function solution(cacheSize, cities) {
     let answer = 0;
-    const cacheMap = new Map()
-    if(cacheSize === 0){
-        return cities.length * 5
-    }else{
-        for(const citi of cities){
-            const parsedCiti = citi.toLowerCase()
-            if(cacheSize <= cacheMap.size){
-                if(cacheMap.has(parsedCiti)){
-                    cacheMap.delete(parsedCiti)     
-                    answer += 1
-                    cacheMap.set(parsedCiti,1)     
-                }else{
-                    answer += 5
-                    const firstKey = cacheMap.keys().next().value;
-                    cacheMap.delete(firstKey);
-                    cacheMap.set(parsedCiti,1)
-                }      
-            }else{
-                if(cacheMap.has(parsedCiti)){
-                    cacheMap.delete(parsedCiti)     
-                    answer += 1
-                    cacheMap.set(parsedCiti,1)     
-                }else{
-                    answer += 5
-                    cacheMap.set(parsedCiti,1)   
-                }   
-            }
-        }
-    }
+    
+    if(cacheSize === 0) return cities.length * 5
+    
+    const map = new Map()
+
+    cities.forEach((v) => {
+        const city = v.toLowerCase()
+        
+        if(map.has(city)) {
+            answer++
+            map.delete(city)
+        } else answer += 5
+        
+        if(map.size === cacheSize) map.delete(map.keys().next().value)
+        
+        map.set(city, true)
+    })
     
     return answer;
 }
